6a1b79a0efb9b960312940a5e9f10ac4fe6c72e7
Li Chen <lichen@youzan.com>
2017-01-16 19:44:14 +0800

    fix: update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-lerna publish --ignore @youzan/zent
-
-# Ensure all packages are up-to-date before publishing this package.
-# Package files will be copied from node_modules
-lerna publish --force-publish=@youzan/zent
+lerna publish --concurrency 1
