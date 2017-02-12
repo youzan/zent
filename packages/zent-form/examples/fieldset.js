55c043b8e365ae03bf791f250c10850b363061ca
niunai <colonD.wny@gmail.com>
2017-02-12 23:37:09 +0800

    feat: form添加fieldset组件

---
+++ packages/zent-form/examples/fieldset.js
@@ -0,0 +1,102 @@
+/* eslint-disable no-console */
+
+import React, { Component } from 'react';
+import { Form, Field, Fieldset, createForm, InputField, CheckboxField, CheckboxGroupField, RadioGroupField, SelectField } from '../src';
+import Checkbox from '@youzan/zent-checkbox';
+import Radio from '@youzan/zent-radio';
+import Option from '@youzan/zent-select';
+import '../assets/index.scss';
+import '@youzan/zent-checkbox/assets/index.scss';
+import '@youzan/zent-select/assets/index.scss';
+import '@youzan/zent-radio/assets/index.scss';
+import '@youzan/zent-input/assets/index.scss';
+
+class FieldsetForm extends Component {
+
+  submit = (values) => {
+    const zentForm = this.props.zentForm;
+
+    console.log(values);
+    console.log(zentForm);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <Form onSubmit={handleSubmit(this.submit)} horizontal>
+        <Fieldset legend="Fieldset1">
+          <Field
+            name="name"
+            type="text"
+            label="用户名："
+            value="123"
+            validations={{ required: true }}
+            validationErrors={{ required: '不能为空' }}
+            component={InputField}
+            helpDesc="我是说明2222"
+            required
+          />
+          <Field
+            name="number"
+            type="number"
+            label="购买数量："
+            addonBefore="买了"
+            value="123"
+            addonAfter="个苹果"
+            validations={{
+              required: true,
+              validNumber(values, value) {
+                return /^\d*$/.test(value);
+              }
+            }}
+            validationErrors={{
+              required: '不能为空',
+              validNumber: '必须是整数'
+            }}
+            component={InputField}
+          />
+        </Fieldset>
+        <Fieldset legend="Fieldset2">
+          <Field
+            name="sex"
+            label="性别："
+            value="female"
+            component={RadioGroupField}
+          >
+            <Radio value="male">男</Radio>
+            <Radio value="female">女</Radio>
+          </Field>
+          <Field
+            name="custom-select"
+            label="请选择："
+            className="custom-select"
+            placeholder="请选择"
+            component={SelectField}
+          >
+            <Option className="zent-select-option" value="1">选项一</Option>
+            <Option className="zent-select-option" value="2">选项二</Option>
+            <Option className="zent-select-option" value="3">选项三</Option>
+          </Field>
+        </Fieldset>
+        <div className="zent-form__form-actions">
+          <button type="submit">提交</button>
+        </div>
+      </Form>
+    );
+  }
+}
+
+const FieldsetFormContainer = createForm()(FieldsetForm);
+
+export default class Simple extends Component {
+  render() {
+    return (
+      <div>
+        <h2>内置表单组件</h2>
+        <hr />
+        <FieldsetFormContainer />
+      </div>
+    );
+  }
+}
---
+++ packages/zent-form/src/Fieldset.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default (props) => {
+  return (
+    <fieldset className="zent-form__fieldset">
+      <legend className="zent-form__legend">{props.legend}</legend>
+      {props.children}
+    </fieldset>
+  );
+}
