56d777a23a77cbe8eade22368e9b855a23f6af82
Li Chen <lichen@youzan.com>
2017-02-19 12:10:03 +0800

    fix: refactor pop arrow offset validation script

--- packages/zent-pop/scripts/validate-pop-size.sh
+++ packages/zent-pop/scripts/validate-pop-size.sh
@@ -8,9 +8,10 @@ fail () {
     exit -1
 }
 
-arrow_size=$(grep -oE '^\s*\$arrow-size\s*:\s*[-+]?[0-9]*\.?[0-9]+' $basepath/../assets/_zent-pop.scss | cut -d ':' -f2)
-arrow_offset=$(grep -oE '^\s*\$arrow-offset\s*:\s*[-+]?[0-9]*\.?[0-9]+' $basepath/../assets/_zent-pop.scss | cut -d ':' -f2)
-js_arrow_offset=$(grep -oE '^\s*\const\s+ARROW_OFFSET\s*=\s*[-+]?[0-9]*\.?[0-9]+' $basepath/../src/position.js | cut -d '=' -f2)
+fp_regexp='[-+]?[0-9]*\.?[0-9]+'
+arrow_size=$(grep -oE "^\s*\\\$arrow-size\s*:\s*$fp_regexp" $basepath/../assets/_zent-pop.scss | cut -d ':' -f2)
+arrow_offset=$(grep -oE "^\s*\\\$arrow-offset\s*:\s*$fp_regexp" $basepath/../assets/_zent-pop.scss | cut -d ':' -f2)
+js_arrow_offset=$(grep -oE "^\s*\const\s+ARROW_OFFSET\s*=\s*$fp_regexp" $basepath/../src/position.js | cut -d '=' -f2)
 is_same=$(echo "scale=2; $arrow_size/2+$arrow_offset==$js_arrow_offset" | bc)
 
 if [ $is_same == 1 ]; then
