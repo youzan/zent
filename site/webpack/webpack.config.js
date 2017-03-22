ebfac5434b6c8e6ab6691bc650ca1d20a9bc2b97
Li Chen <lichen@youzan.com>
2017-03-22 15:19:57 +0800

    fix: url

--- site/webpack/webpack.config.js
+++ site/webpack/webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, '../dist'),
-    publicPath: 'dist/',
+    publicPath: '/dist/',
     filename: '[name].js'
   },
   resolve: {
