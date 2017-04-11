5580accd9f4356cac844f9b326c5bc0bc90e5f24
Li Chen <lichen@youzan.com>
2017-04-11 20:14:44 +0800

    doc: add tool to generate change log

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -17,6 +17,14 @@ if ! command_exists superman ; then
     fail 'superman is required to publish packages'
 fi
 
+if !command_exists github_changelog_generator ; then
+    fail 'github_changelog_generator is required to publish packages'
+fi
+
+if [ -z "$CHANGELOG_GITHUB_TOKEN" ] ; then
+    fail 'You must set CHANGELOG_GITHUB_TOKEN environment variable\nhttps://github.com/skywinder/github-changelog-generator#github-token'
+fi
+
 # 重新bootstrap，以防有人改了组件的依赖
 $basepath/../lerna clean --yes
 $basepath/../lerna bootstrap
@@ -24,5 +32,8 @@ $basepath/../lerna bootstrap
 # 循序执行，因为zent依赖其他包prepublish后的结果，会比较慢
 $basepath/../lerna publish --exact --concurrency 1 --npm-tag=$npmtag
 
-# 确保字体文件已经上传CDN
-# $basepath/../packages/zent-icon/scripts/check_cdn.sh
+# 生成 change log
+github_changelog_generator --since-tag @youzan/zent@0.4.7 --exclude-tags-regex "zent-.+" -o $basepath/../packages/zent/CHANGELOG.md
+git add $basepath/../packages/zent/CHANGELOG.md
+git commit -m "doc: update change log(publish)"
+git push
