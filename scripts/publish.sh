13d4f6f2359b96ddfa5966f33db458fcf392f16f
Li Chen <lichen@youzan.com>
2017-04-27 14:02:56 +0800

    fix: update scripts

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -12,10 +12,6 @@ command_exists () {
     type "$1" >/dev/null 2>&1
 }
 
-if ! command_exists superman ; then
-    fail 'superman is required to publish packages'
-fi
-
 if ! command_exists github_changelog_generator ; then
     fail 'github_changelog_generator is required to publish packages'
 fi
@@ -24,12 +20,11 @@ if [ -z "$CHANGELOG_GITHUB_TOKEN" ] ; then
     fail 'You must set CHANGELOG_GITHUB_TOKEN environment variable\nhttps://github.com/skywinder/github-changelog-generator#github-token'
 fi
 
-# 重新bootstrap，以防有人改了组件的依赖
+# 重新bootstrap，以防有人改了依赖
 $basepath/../lerna clean --yes
 $basepath/../lerna bootstrap
 
-# 循序执行，因为zent依赖其他包prepublish后的结果，会比较慢
-$basepath/../lerna publish --exact --concurrency 1 "$@"
+$basepath/../lerna publish --exact "$@"
 
 # 生成 change log
 github_changelog_generator \
@@ -43,5 +38,5 @@ github_changelog_generator \
     -o $basepath/../packages/zent/CHANGELOG.md
 
 git add $basepath/../packages/zent/CHANGELOG.md
-git commit -m "doc: update change log(publish)"
+git commit -m "doc: update change log (publish)"
 git push
