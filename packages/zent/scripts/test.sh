e3f9cf02c9b5360687fce7d7288687e89f8c50db
Li Chen <lichen@youzan.com>
2017-05-04 17:07:31 +0800

    add coverage

--- packages/zent/scripts/test.sh
+++ packages/zent/scripts/test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+basepath=$(dirname $0)
+
 if [[ -z "${CI}" ]]; then
   jest -c jest.config.json "$@"
 else
   jest -c jest.config.json --runInBand "$@"
+  cat $basepath/../coverage/lcov.info | $basepath/../node_modules/.bin/coveralls
 fi
