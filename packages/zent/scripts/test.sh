71634774a90f6835dfa9a2b2bb76ea6e9fc3d6ce
Li Chen <lichen@youzan.com>
2017-05-04 17:16:36 +0800

    Only upload coverage info on travis

--- packages/zent/scripts/test.sh
+++ packages/zent/scripts/test.sh
@@ -6,5 +6,9 @@ if [[ -z "${CI}" ]]; then
   jest -c jest.config.json "$@"
 else
   jest -c jest.config.json --runInBand "$@"
+fi
+
+# Only upload if we're on travis
+if [[ -n "${TRAVIS}" ]]; then
   cat $basepath/../coverage/lcov.info | $basepath/../node_modules/.bin/coveralls
 fi
