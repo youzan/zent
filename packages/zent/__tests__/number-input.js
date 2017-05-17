46cfe1015b4523b1479a547617e6bf6d552c49c4
zhuxiang <zhuxiang@qima-inc.com>
2017-05-17 10:22:01 +0800

    change component name

--- packages/zent/__tests__/number-input.js
+++ packages/zent/__tests__/number-input.js
@@ -1,35 +1,35 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import Numinput from 'numinput';
+import NumberInput from 'number-input';
 
-describe('Numinput', () => {
+describe('NumberInput', () => {
   it('will render div wrapper contains an Input component', () => {
-    const wrapper = shallow(<Numinput />);
+    const wrapper = shallow(<NumberInput />);
     expect(wrapper.type()).toBe('div');
-    expect(wrapper.hasClass('zent-numinput-wrapper')).toBe(true);
+    expect(wrapper.hasClass('zent-number-input-wrapper')).toBe(true);
     expect(wrapper.find('.zent-input-wrapper')).to.have.length(1);
-    expect(wrapper.find('.zent-numinput-wrapper').childAt(0).type()).toBe(
+    expect(wrapper.find('.zent-number-input-wrapper').childAt(0).type()).toBe(
       'span'
     );
-    expect(wrapper.find('.zent-numinput-wrapper').childAt(2).type()).toBe(
+    expect(wrapper.find('.zent-number-input-wrapper').childAt(2).type()).toBe(
       'span'
     );
   });
 
   it('can have custom wrapper classNames', () => {
-    const wrapper = shallow(<Numinput className="foo" />);
+    const wrapper = shallow(<NumberInput className="foo" />);
     expect(wrapper.hasClass('foo')).toBe(true);
     expect(wrapper.find('input').hasClass('foo')).toBe(false);
   });
 
   it('can have custom prefix of classNames', () => {
-    const wrapper = shallow(<Numinput prefix="foo" />);
-    expect(wrapper.hasClass('foo-numinput-wrapper')).toBe(true);
+    const wrapper = shallow(<NumberInput prefix="foo" />);
+    expect(wrapper.hasClass('foo-number-input-wrapper')).toBe(true);
   });
 
   it('pass any props to real input element except "className" & "prefix"', () => {
     let wrapper = shallow(
-      <Numinput min={8} max={11} decimal={2} className="foo" />
+      <NumberInput min={8} max={11} decimal={2} className="foo" />
     );
     expect(wrapper.find('input').props().className).toBe('zent-input');
     expect(wrapper.find('input').props().prefix).toBe(undefined);
@@ -37,16 +37,16 @@ describe('Numinput', () => {
     expect(wrapper.props().max).toBe(11);
     expect(wrapper.props().min).toBe(8);
     expect(wrapper.props().decimal).toBe(2);
-    wrapper = shallow(<Numinput placeholder="default" disabled />);
+    wrapper = shallow(<NumberInput placeholder="default" disabled />);
     expect(wrapper.find('input').props().placeholder).toBe('default');
     expect(wrapper.find('input').props().disabled).toBe(true);
   });
 
-  it('Numinput has its core function, change value with click on arrow', () => {
-    let wrapper = mount(<Numinput type="count" value={2} />);
-    wrapper.find('.zent-numinput-arrow').at(1).simulate('click');
+  it('NumberInput has its core function, change value with click on arrow', () => {
+    let wrapper = mount(<NumberInput type="count" value={2} />);
+    wrapper.find('.zent-number-input-arrow').at(1).simulate('click');
     expect(wrapper.prop('value')).toBe(3);
-    wrapper.find('.zent-numinput-arrow').at(2).simulate('click');
+    wrapper.find('.zent-number-input-arrow').at(2).simulate('click');
     expect(wrapper.prop('value')).toBe(2);
   });
 });
