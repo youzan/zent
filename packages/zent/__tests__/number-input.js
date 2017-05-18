eb6edaf5ad85d4fe89eba746a6004cfdc23ea7c9
zhuxiang <zhuxiang@qima-inc.com>
2017-05-18 14:28:08 +0800

    done

--- packages/zent/__tests__/number-input.js
+++ packages/zent/__tests__/number-input.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import TestUtils from 'react-dom/test-utils';
 import NumberInput from 'number-input';
 
 describe('NumberInput', () => {
@@ -19,6 +20,26 @@ describe('NumberInput', () => {
     expect(wrapper.hasClass('foo-number-input-wrapper')).toBe(true);
   });
 
+  it('change value within min and max ', () => {
+    let wrapper = shallow(
+      <NumberInput showStepper value={2} min={0} max={3} />
+    );
+    wrapper.find('.zent-number-input-arrowup').simulate('click');
+    wrapper.find('.zent-number-input-arrowup').simulate('click');
+    wrapper.find('.zent-number-input-arrowup').simulate('click');
+    wrapper.find('.zent-number-input-arrowup').simulate('click');
+    expect(wrapper.state('value')).toBe('3');
+    wrapper.find('.zent-number-input-arrowdown').simulate('click');
+    wrapper.find('.zent-number-input-arrowdown').simulate('click');
+    wrapper.find('.zent-number-input-arrowdown').simulate('click');
+    wrapper.find('.zent-number-input-arrowdown').simulate('click');
+    expect(wrapper.state('value')).toBe('0');
+    wrapper = shallow(<NumberInput showStepper value={0} min={1} max={3} />);
+    expect(wrapper.state('value')).toBe('1');
+    wrapper = shallow(<NumberInput showStepper value={6} min={0} max={3} />);
+    expect(wrapper.state('value')).toBe('3');
+  });
+
   it('NumberInput has its core function, change value with click on arrow', () => {
     let wrapper = mount(<NumberInput showStepper value={2} />);
     const onChangeMock = jest.fn();
@@ -28,8 +49,6 @@ describe('NumberInput', () => {
     });
     wrapper = mount(
       <NumberInput
-        max={1}
-        min={0}
         onChange={onChangeMock}
         onBlur={onBlurMock}
         showStepper
@@ -37,9 +56,10 @@ describe('NumberInput', () => {
       />
     );
     wrapper.find('.zent-number-input-arrowup').simulate('click');
-    expect(wrapper.state('value')).toBe('1');
+    expect(wrapper.state('value')).toBe('3');
     wrapper.find('.zent-number-input-arrowdown').simulate('click');
-    expect(wrapper.state('value')).toBe('0');
+    expect(wrapper.state('value')).toBe('2');
+
     wrapper.find('input').simulate('change');
     expect(onChangeMock.mock.calls.length).toBe(2);
     wrapper.find('input').simulate('blur');
@@ -47,5 +67,43 @@ describe('NumberInput', () => {
     wrapper = mount(<NumberInput min={0} showStepper value={-1} />);
     wrapper.find('.zent-number-input-arrowdown').simulate('click');
     expect(wrapper.state('value')).toBe('0');
+  });
+
+  it('NumberInput onchange value', () => {
+    // let wrapper = mount(<NumberInput showStepper value={2} />);
+    let preventDefaultCalled = false;
+    let stopPropagationCalled = false;
+
+    const handleChange = e => {
+      expect(e.target.value).toBe('');
+
+      expect(preventDefaultCalled).toBe(false);
+      expect(stopPropagationCalled).toBe(false);
+
+      e.preventDefault();
+      e.stopPropagation();
+
+      expect(preventDefaultCalled).toBe(true);
+      expect(stopPropagationCalled).toBe(true);
+    };
+
+    const wrapper = TestUtils.renderIntoDocument(
+      <NumberInput onChange={handleChange} value={1} />
+    );
+
+    const inputNode = TestUtils.findRenderedDOMComponentWithTag(
+      wrapper,
+      'input'
+    );
+
+    TestUtils.Simulate.change(inputNode, {
+      target: { value: '' },
+      preventDefault() {
+        preventDefaultCalled = true;
+      },
+      stopPropagation() {
+        stopPropagationCalled = true;
+      }
+    });
   });
 });
