451212228e1c73f8b7ef218dc0033b7366b0c028
jinzhixin <hzjzxpnw@outlook.com>
2017-05-30 22:44:57 +0800

    checkbox

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -166,6 +166,31 @@ declare namespace Zent {
 
   class Button extends React.Component<IButtonProps, any> {}
 
+  interface ICheckBoxProps {
+    checked?: boolean
+    value?: any
+    disabled?: boolean
+    indeterminate?: boolean
+    onChange?: React.ChangeEventHandler<HTMLInputElement>
+    className?: string
+    prefix?: string
+  }
+
+  interface ICheckBoxGroupProps {
+    value: Array<any>
+    isValueEqual?: (value1: any, value2: any) => boolean
+    disabled?: boolean
+    onChange?: (values: Array<any>) => void
+    className?: string
+    prefix?: string
+  }
+
+  class CheckBox extends React.Component<ICheckBoxProps, any> {}
+
+  namespace CheckBox {
+    class Group extends React.Component<ICheckBoxGroupProps, any> {}
+  }
+
   
 }
 
