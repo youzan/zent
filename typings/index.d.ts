5f4d2c4269f8d31d64b99039cea21814d8861209
jinzhixin <hzjzxpnw@outlook.com>
2017-05-31 01:08:20 +0800

    update

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -389,7 +389,35 @@ declare namespace Zent {
     class Step extends React.Component<IStepProps, any> {}
   }
 
-  
+  interface ITableColumn {
+    title: string
+    name: string
+    width?: number
+    textAign?: 'left'|'right'|'center'
+    isMoney?: boolean
+    bodyRender?: (data: any) => React.ReactNode
+  }
+
+  interface ITableProps {
+    columns: Array<ITableColumn>
+    datasets: Array<Object>
+    rowKey?: string
+    sortBy?: string
+    sortType?: 'desc'|'asc'
+    onChange?: (conf: any) => void
+    emptyLabel?: string
+    selection?: { selectedRowKeys?: Array<string>, onSelect?: (selectedkeys: string, selectedRows: Array<any>, currentRow: number) => void }
+    loading?: boolean
+    getRowConf?: (data: Object, index: number) => { canSelect: boolean, rowClass: string }
+    expandation?: { isExpanded?: boolean, expandRender?: (data: any) => React.ReactNode }
+    autoStick?: boolean
+    autoScroll?: boolean
+    className?: string
+    prefix?: string
+  }
+
+  class Table extends React.Component<ITableProps, any> {}
+
 }
 
 
