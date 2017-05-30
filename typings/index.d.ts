f02257fa719e238bea97b8c97636824dba64bdb6
jinzhixin <hzjzxpnw@outlook.com>
2017-05-31 01:30:31 +0800

    update

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -441,7 +441,40 @@ declare namespace Zent {
 
   class Tabs extends React.Component<ITabsProps, any> {}
 
-  
+  interface ITreeData {
+    id: number|string
+    title: number|string
+    children?: Array<ITreeData>
+    parendId?: string|number
+    expand?: boolean
+    isLeaf?: boolean
+  }
+
+  interface ITreeOperation {
+    name: string
+    icon?: string|React.ReactElement<any>
+    action: (data: ITreeData) => void
+    shouldRender?: (data: ITreeData) => boolean
+  }
+
+  interface ITreeProps {
+    dataType?: 'tree'|'plain'
+    data: Array<ITreeData>
+    render?: (data: ITreeData) => React.ReactNode
+    operations?: Array<ITreeOperation>
+    foldable?: boolean
+    checkable?: boolean
+    onCheck?: (data: Array<number|string>) => void
+    defaultCheckedKeys?: Array<number|string>
+    disabledCheckedKeys?: Array<number|string>
+    size?: 'medium'|'small'|'large'
+    commonStyle?: React.CSSProperties
+    expandAll?: boolean
+    onExpand?: (data: ITreeData, config: { isExpanded: boolean }) => void
+    autoExpandOnSelect?: boolean
+    onSelect?: (data: ITreeData, target: HTMLSpanElement) => void
+    isRoot?: (data: ITreeData) => boolean
+  }
 
 }
 
