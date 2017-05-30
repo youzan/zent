9af5e81a84e6a523e43250a379eb618949fc6a86
jinzhixin <hzjzxpnw@outlook.com>
2017-05-30 23:51:15 +0800

    radio

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -245,7 +245,25 @@ declare namespace Zent {
     focus()
   }
 
-  
+  interface IRadioProps {
+    value: any
+    className?: string
+    prefix?: string
+  }
+
+  class Radio extends React.Component<IRadioProps, any> {}
+
+  namespace Radio {
+    interface IGroupProps {
+      value: any
+      onChange: React.ChangeEventHandler<HTMLInputElement>
+      isValueEqual?: (value1: any, value2: any) => boolean
+      className?: string
+      prefix?: string
+    }
+
+    class Group extends React.Component<IGroupProps, any> {}
+  }
 }
 
 
