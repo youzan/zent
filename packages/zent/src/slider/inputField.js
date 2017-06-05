09de2dc4d1a58b6c78e6359e13450adb70a4ff38
wulv <543885224@qq.com>
2017-06-05 14:52:43 +0800

    fix: replace number input

--- packages/zent/src/slider/inputField.js
+++ packages/zent/src/slider/inputField.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import NumberInput from './numberInput';
+// import NumberInput from './numberInput';
+import NumberInput from 'number-input';
+
+import { getDecimal } from './common';
 
 export default class InputField extends Component {
-  onchange = (type, newValue) => {
+  onchange = (type, e) => {
     const { onChange, value } = this.props;
+    let newValue = Number(e.target.value);
     if (type === 'start') {
       newValue = [newValue, value[1]];
     } else if (type === 'end') {
@@ -20,18 +24,23 @@ export default class InputField extends Component {
           ? <div className={`${prefix}-slider-input`}>
               <NumberInput
                 {...restProps}
+                max={value[1]}
+                decimal={getDecimal(restProps.step)}
                 onChange={this.onchange.bind(null, 'start')}
                 value={value[0]}
               />
               <span className="slider-input-line">-</span>
               <NumberInput
                 {...restProps}
+                min={value[0]}
+                decimal={getDecimal(restProps.step)}
                 onChange={this.onchange.bind(null, 'end')}
                 value={value[1]}
               />
             </div>
           : <NumberInput
               {...restProps}
+              decimal={getDecimal(restProps.step)}
               onChange={this.onchange.bind(null, 'single')}
               value={value}
             />}
