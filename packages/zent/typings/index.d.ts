41473f993180dd167a4483f9f399952021af6d3d
jinzhixin <hzjzxpnw@outlook.com>
2017-08-25 13:56:36 +0800

    improve types

--- packages/zent/typings/index.d.ts
+++ packages/zent/typings/index.d.ts
@@ -3,15 +3,141 @@
 /// <reference types="react" />
 
 /// <reference path="./libs/Affix.d.ts" />
-/// <reference path="./libs/Layout.d.ts" />
+/// <reference path="./libs/Alert.d.ts" />
+/// <reference path="./libs/Badge.d.ts" />
+/// <reference path="./libs/BlockHeader.d.ts" />
+/// <reference path="./libs/Breadcrumb.d.ts" />
+/// <reference path="./libs/Button.d.ts" />
+/// <reference path="./libs/Card.d.ts" />
+/// <reference path="./libs/Cascader.d.ts" />
+/// <reference path="./libs/CheckBox.d.ts" />
+/// <reference path="./libs/ColorPicker.d.ts" />
+/// <reference path="./libs/CopyButton.d.ts" />
+/// <reference path="./libs/DateRangeQuickPicker.d.ts" />
+/// <reference path="./libs/Design.d.ts" />
+/// <reference path="./libs/Dialog.d.ts" />
+/// <reference path="./libs/Form.d.ts" />
 /// <reference path="./libs/Icon.d.ts" />
+/// <reference path="./libs/Input.d.ts" />
+/// <reference path="./libs/Layout.d.ts" />
+/// <reference path="./libs/Loading.d.ts" />
+/// <reference path="./libs/Menu.d.ts" />
+/// <reference path="./libs/Notify.d.ts" />
+/// <reference path="./libs/NumberInput.d.ts" />
+/// <reference path="./libs/Pagination.d.ts" />
+/// <reference path="./libs/Pop.d.ts" />
+/// <reference path="./libs/Popover.d.ts" />
+/// <reference path="./libs/Portal.d.ts" />
+/// <reference path="./libs/previewImage.d.ts" />
+/// <reference path="./libs/Progress.d.ts" />
+/// <reference path="./libs/Radio.d.ts" />
+/// <reference path="./libs/SearchInput.d.ts" />
+/// <reference path="./libs/Select.d.ts" />
+/// <reference path="./libs/SKU.d.ts" />
+/// <reference path="./libs/Slider.d.ts" />
+/// <reference path="./libs/Steps.d.ts" />
+/// <reference path="./libs/SweetAlert.d.ts" />
+/// <reference path="./libs/Swiper.d.ts" />
+/// <reference path="./libs/Switch.d.ts" />
+/// <reference path="./libs/Table.d.ts" />
+/// <reference path="./libs/Tabs.d.ts" />
+/// <reference path="./libs/Tree.d.ts" />
+/// <reference path="./libs/Upload.d.ts" />
 
 declare module 'zent' {
-  import Affix from 'zent/lib/affix'
-  import Layout from 'zent/lib/layout'
-
+  import Affix from 'zent/lib/affix';
+  import Alert from 'zent/lib/alert';
+  import Badge from 'zent/lib/badge';
+  import BlockHeader from 'zent/lib/block-header';
+  import Breadcrumb from 'zent/lib/breadcrumb';
+  import Button from 'zent/lib/button';
+  import Cascader from 'zent/lib/cascader';
+  import Card from 'zent/lib/card';
+  import Checkbox from 'zent/lib/checkbox';
+  import ColorPicker from 'zent/lib/colorpicker';
+  import CopyButton from 'zent/lib/copy-button';
+  // import DatePicker from 'datetimepicker/DatePicker';
+  // import DateRangeQuickPicker from 'date-range-quick-picker';
+  // import MonthPicker from 'datetimepicker/MonthPicker';
+  // import DateRangePicker from 'datetimepicker/DateRangePicker';
+  import Design from 'zent/lib/design';
+  import Dialog from 'zent/lib/dialog';
+  import Form from 'zent/lib/form';
+  import Layout from 'zent/lib/layout';
+  import Loading from 'zent/lib/loading';
+  import Icon from 'zent/lib/icon';
+  import Input from 'zent/lib/input';
+  import NumberInput from 'zent/lib/number-input';
+  import Menu from 'zent/lib/menu';
+  import Notify from 'zent/lib/notify';
+  import Pagination from 'zent/lib/pagination';
+  import Pop from 'zent/lib/pop';
+  import Popover from 'zent/lib/popover';
+  import Portal from 'zent/lib/portal';
+  import previewImage from 'zent/lib/preview-image';
+  import Progress from 'zent/lib/progress';
+  import Radio from 'zent/lib/radio';
+  import SearchInput from 'zent/lib/search-input';
+  import Select from 'zent/lib/select';
+  import SKU from 'zent/lib/sku';
+  import Slider from 'zent/lib/slider';
+  import Steps from 'zent/lib/steps';
+  import Sweetalert from 'zent/lib/sweetalert';
+  import Swiper from 'zent/lib/swiper';
+  import Switch from 'zent/lib/switch';
+  import Table from 'zent/lib/table';
+  import Tabs from 'zent/lib/tabs';
+  import Tag from 'zent/lib/tag';
+  import Tree from 'zent/lib/tree';
+  import Upload from 'zent/lib/upload';
+  
   export {
-    Affix
-  }
+    Affix,
+    Alert,
+    Badge,
+    BlockHeader,
+    Breadcrumb,
+    Button,
+    Cascader,
+    Card,
+    Checkbox,
+    ColorPicker,
+    CopyButton,
+    // DatePicker,
+    // DateRangePicker,
+    // DateRangeQuickPicker,
+    Design,
+    Dialog,
+    Form,
+    Layout,
+    Loading,
+    Icon,
+    Input,
+    NumberInput,
+    Menu,
+    // MonthPicker,
+    Notify,
+    Pagination,
+    Pop,
+    Popover,
+    Portal,
+    previewImage,
+    Progress,
+    Radio,
+    SearchInput,
+    Select,
+    SKU,
+    Slider,
+    Steps,
+    Sweetalert,
+    Swiper,
+    Table,
+    Tabs,
+    Tag,
+    Tree,
+    Switch,
+    Upload
+  };
+  
 }
 
