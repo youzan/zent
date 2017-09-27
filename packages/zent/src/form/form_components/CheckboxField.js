6074db8bae84603cf1b0b130b8274a0b26075921
dengwenbo <tiamoarvin@hotmail.com>
2017-09-27 15:13:32 +0800

    fix: Form 修复ControlGroup内stateless component的react 16warning

--- packages/zent/src/form/form_components/CheckboxField.js
+++ packages/zent/src/form/form_components/CheckboxField.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Component, PureComponent } from 'react';
 import Checkbox from 'checkbox';
 import omit from 'lodash/omit';
 
 import getControlGroup from '../getControlGroup';
 import unknownProps from '../unknownProps';
 
-const CheckboxWrap = props => {
-  const passableProps = omit(props, unknownProps);
-  return (
-    <Checkbox
-      className="zent-form__checkbox"
-      checked={props.value === true}
-      {...passableProps}
-    />
-  );
-};
+class CheckboxWrap extends (PureComponent || Component) {
+  render() {
+    const passableProps = omit(this.props, unknownProps);
+    return (
+      <Checkbox
+        className="zent-form__checkbox"
+        checked={this.props.value === true}
+        {...passableProps}
+      />
+    );
+  }
+}
+
 const CheckboxField = getControlGroup(CheckboxWrap);
 
 export default CheckboxField;
