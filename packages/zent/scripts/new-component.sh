43820cf34f723f80744b050dc69324ff396d5e10
ronghang <ronghang@youzan.com>
2017-10-11 21:09:42 +0800

    fix shell

--- packages/zent/scripts/new-component.sh
+++ packages/zent/scripts/new-component.sh
@@ -5,13 +5,13 @@ js_file_path=$basepath/../src/$1/index.js
 assets_index_path=$basepath/../assets/index.pcss
 js_file_name="$1.js"
 css_file_name="$1.pcss"
-
-echo "开始初始化组件：$@"
+upper_name="$(tr '[:lower:]' '[:upper:]' <<< ${1:0:1})${1:1}"
+upper_js_file_name="$upper_name.js"
 
 function touch_js_file () {
   touch index.js
   touch README.md
-  touch $js_file_name
+  touch $upper_js_file_name
 }
 
 function touch_css_file () {
@@ -20,7 +20,7 @@ function touch_css_file () {
 
 # 在 index.js 中导出
 function update_js_index () {
-  echo "export default from './$js_file_name';" > $js_file_path
+  echo "export default from './$upper_js_file_name';" > $js_file_path
 }
 
 # 在 assets/index.pcss 中import
@@ -28,14 +28,19 @@ function update_assets_index () {
   echo "@import './$css_file_name';" >> $assets_index_path
 }
 
-# js代码
-cd $basepath/../src && mkdir $1 && cd $1 && touch_js_file && cd ../../ && update_js_index
+if [ ! -z "$1" ] ; then
+  echo "开始初始化组件：$upper_name"
+  # js代码
+  cd $basepath/../src && mkdir $1 && cd $1 && touch_js_file && cd ../../ && update_js_index
 
-# css代码
-cd $basepath/../assets && touch_css_file && cd ../ && update_assets_index
+  # css代码
+  cd $basepath/../assets && touch_css_file && cd ../ && update_assets_index
 
-echo "完成初始化组件：$@"
+  echo "完成初始化组件：$upper_name"
 
-echo '更新jest配置...'
+  echo '更新jest配置...'
 
-node $basepath/update-jest-module-mapper.js
+  node $basepath/update-jest-module-mapper.js
+else
+  echo '请输入需要新建的组件名！'
+fi
