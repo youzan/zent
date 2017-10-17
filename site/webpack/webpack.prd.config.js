fccea2723fde02bba601afce6ffa8110afe55da4
fancy <lucasicarus@hotmail.com>
2017-10-17 16:52:43 +0800

    dev: project init with upgrade npm dependencies

--- site/webpack/webpack.prd.config.js
+++ site/webpack/webpack.prd.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const vendorEntry = require('./vendor-entry');
 const base = require('./webpack.config');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+// const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const { getBabelLoader, postcssLoader, getRules } = require('./loader.config');
 
 const babelLoader = getBabelLoader({ dev: false });
@@ -32,39 +32,39 @@ module.exports = Object.assign({}, base, {
   }),
 
   plugins: base.plugins.concat([
-    new FaviconsWebpackPlugin({
-      // Your source logo
-      logo: './assets/ZanUIlogo256x256.png',
-      // The prefix for all image files (might be a folder or a name)
-      prefix: 'favico-[hash]-',
-      // Emit all stats of the generated icons
-      // emitStats: false,
-      // The name of the json containing all favicon information
-      // statsFilename: 'iconstats-[hash].json',
-      // Generate a cache file with control hashes and
-      // don't rebuild the favicons until those hashes change
-      persistentCache: false,
-      // Inject the html into the html-webpack-plugin
-      inject: true,
-      // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
-      background: '#fff',
-      // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
-      title: 'Zent',
+    // new FaviconsWebpackPlugin({
+    //   // Your source logo
+    //   logo: './assets/ZanUIlogo256x256.png',
+    //   // The prefix for all image files (might be a folder or a name)
+    //   prefix: 'favico-[hash]-',
+    //   // Emit all stats of the generated icons
+    //   // emitStats: false,
+    //   // The name of the json containing all favicon information
+    //   // statsFilename: 'iconstats-[hash].json',
+    //   // Generate a cache file with control hashes and
+    //   // don't rebuild the favicons until those hashes change
+    //   persistentCache: false,
+    //   // Inject the html into the html-webpack-plugin
+    //   inject: true,
+    //   // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
+    //   background: '#fff',
+    //   // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
+    //   title: 'Zent',
 
-      // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
-      icons: {
-        android: true,
-        appleIcon: true,
-        appleStartup: true,
-        coast: false,
-        favicons: true,
-        firefox: true,
-        opengraph: false,
-        twitter: false,
-        yandex: false,
-        windows: false
-      }
-    }),
+    //   // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+    //   icons: {
+    //     android: true,
+    //     appleIcon: true,
+    //     appleStartup: true,
+    //     coast: false,
+    //     favicons: true,
+    //     firefox: true,
+    //     opengraph: false,
+    //     twitter: false,
+    //     yandex: false,
+    //     windows: false
+    //   }
+    // }),
 
     new webpack.optimize.UglifyJsPlugin({
       compress: {
