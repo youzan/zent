89fca3ae6b3a634321f82882741dec3411a4aa53
huangshiyu@youzan.com <huangshiyu@youzan.com>
2017-11-27 11:46:29 +0800

    feat: 删掉不需要的代码

--- packages/zent/src/upload/utils/index.js
+++ packages/zent/src/upload/utils/index.js
@@ -50,45 +50,6 @@ export function formatDuration(duration) {
   return result.join(':');
 }
 
-export function responseParse(file) {
-  let {
-    attachmentFullUrl,
-    attachmentId,
-    attachmentSize,
-    attachmentTitle,
-    attachmentUrl,
-    categoryId,
-    createTime,
-    fileExt,
-    isDelete,
-    kdtId,
-    mediaExpireTime,
-    mediaId,
-    mediaType,
-    thumbUrl,
-    ...rest
-  } = file;
-  return {
-    attachment_file: attachmentUrl || file.attachment_file,
-    attachment_full_url: attachmentFullUrl || file.attachment_full_url,
-    attachment_id: `${attachmentId}` || file.attachment_id,
-    attachment_size: `${attachmentSize}` || file.attachment_size,
-    attachment_title: attachmentTitle || file.attachment_title,
-    attachment_url: attachmentFullUrl || file.attachment_url,
-    category_id: categoryId || file.category_id,
-    create_time: createTime || file.create_time,
-    file_ext: fileExt || file.file_ext,
-    is_delete: `${isDelete}` || file.is_delete,
-    kdt_id: `${kdtId}` || file.kdt_id,
-    media_expire_time: mediaExpireTime || file.media_expire_time,
-    media_id: `${mediaId}` || file.media_id,
-    media_type: mediaType || file.media_type,
-    thumb_file: attachmentUrl || file.thumb_file,
-    thumb_url: thumbUrl || file.thumb_url,
-    ...rest
-  };
-}
-
 export function base64ToArrayBuffer(base64) {
   let binary_string = window.atob(base64);
   let len = binary_string.length;
