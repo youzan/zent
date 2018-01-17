7d183e9752b4a76b1af369995153eb9440d37a56
fancy <lucasicarus@hotmail.com>
2018-01-18 00:11:35 +0800

    dev: unify the trigger of select, maybe cause test cases not pass

--- packages/zent/src/select/trigger/index.js
+++ packages/zent/src/select/trigger/index.js
@@ -1,20 +1,55 @@
+import * as React from 'react';
 import PropTypes from 'prop-types';
-import React, { Component, PureComponent } from 'react';
 
-class Trigger extends (PureComponent || Component) {
-  componentWillReceiveProps(nextProps) {
-    this.setState(nextProps);
-  }
+import Popover from 'popover';
 
-  render() {
-    const Node = this.props.trigger;
+import Input from './InputTrigger';
+import Base from './BaseTrigger';
+import Simple from './SimpleTrigger';
+import Tags from './TagsTrigger';
 
-    return <Node {...this.props} onKeyDown={this.props.onKeyDown} />;
-  }
-}
-
-Trigger.propTypes = {
-  trigger: PropTypes.any
+// Compatible with old APIs. e.g. props.simple / props.search
+const NodeMap = {
+  Input,
+  Base,
+  Simple,
+  Tags
 };
 
-export default Trigger;
+/**
+ * @description exclusive click trigger of select
+ * @class SelectClickTrigger
+ * @extends {Popover.Trigger.Click}
+ * @method {render} Major changes, Omit the check and React.cloneElement.
+ * @method {clickHandler} Click event Agent
+ */
+export default class SelectClickTrigger extends Popover.Trigger.Click {
+  static propTypes = {
+    disabled: PropTypes.bool.isRequired,
+    open: PropTypes.func.isRequired,
+    close: PropTypes.func.isRequired,
+    contentVisible: PropTypes.bool.isRequired,
+    onClick: PropTypes.func.isRequired,
+    trigger: PropTypes.oneOfType([PropTypes.string, PropTypes.func]).isRequired,
+    onKeyDown: PropTypes.func.isRequired
+  };
+
+  clickHandler = event => {
+    const { onClick, close, open, contentVisible, disabled } = this.props;
+    event.preventDefault();
+    if (contentVisible) {
+      close();
+    } else if (!disabled) {
+      open();
+      onClick(event);
+    }
+  };
+
+  render() {
+    const { onClick, trigger, onTriggerRefChange, ...rest } = this.props;
+    const Node = typeof trigger === 'string' ? NodeMap[trigger] : trigger;
+    return (
+      <Node {...rest} ref={onTriggerRefChange} onClick={this.clickHandler} />
+    );
+  }
+}
