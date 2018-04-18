c179c6004261734c1b451b234949e809cf4bcfca
Li Chen <lichen@youzan.com>
2018-04-18 15:16:47 +0800

    Update CI scripts

--- scripts/check-git-hooks.sh
+++ scripts/check-git-hooks.sh
@@ -19,6 +19,6 @@ rv=$?
 popd >/dev/null 2>&1
 
 if [ $rv -ne 0 ]; then
-  git diff --name-only
+  git diff --name-only --exit-code
   fail 'Git hooks not installed. Follow these instructions on your local machine:\n1. yarn install\n2. yarn prettify\n3. Commit your changes and push.'
 fi
