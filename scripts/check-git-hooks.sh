121b65e5df67003845388d8ca19930ebb0752068
Li Chen <lichen@youzan.com>
2018-04-18 17:50:40 +0800

    Update

--- scripts/check-git-hooks.sh
+++ scripts/check-git-hooks.sh
@@ -14,13 +14,12 @@ fail () {
 
 pushd $basepath/.. >/dev/null 2>&1
 yarn prettify
-git diff-index HEAD --
+git diff-index --quite HEAD --
 rv=$?
 echo $rv
-git diff --name-only --exit-code
 popd >/dev/null 2>&1
 
 if [ $rv -ne 0 ]; then
-  git diff --name-only --exit-code
+  git diff-index HEAD --
   fail 'Git hooks not installed. Follow these instructions on your local machine:\n1. yarn install\n2. yarn prettify\n3. Commit your changes and push.'
 fi
