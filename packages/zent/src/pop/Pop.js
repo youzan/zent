9b14df19fa50cbc858ca0ec1ac814d072c74817a
Charles Lee <cpylua@users.noreply.github.com>
2018-07-20 10:00:24 +0800

    Update Pop.js

--- packages/zent/src/pop/Pop.js
+++ packages/zent/src/pop/Pop.js
@@ -173,6 +173,7 @@ class Pop extends PureComponent {
 
     // 只有trigger为click时才有效
     closeOnClickOutside: PropTypes.bool,
+
     isOutside: PropTypes.func,
 
     // 在 popover-content 进入屏幕内时触发, 生命周期内仅触发一次
