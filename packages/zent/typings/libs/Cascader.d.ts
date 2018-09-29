58e7e4229467fcf6ec884f152a71c199927cf9d0
Big Mogician <sunaoran@youzan.com>
2018-09-29 17:36:24 +0800

    优化Cascader组件定义

--- packages/zent/typings/libs/Cascader.d.ts
+++ packages/zent/typings/libs/Cascader.d.ts
@@ -2,34 +2,40 @@
 
 declare module 'zent/lib/cascader' {
 
-  interface ICascaderItem {
-    id: any;
-    title: any;
+  namespace Cascader {
+
+    export interface IItem<T = any> {
+      id: T;
+      title: any;
+    }
+
+    export interface IOption<T = any> extends IItem<T> {
+      children?: IOption<T>[]
+    }
+
+    export interface ILoadMoreResolvedItem<T = any> extends IItem<T> {
+      isLeaf: boolean
+    }
+
+    export interface IProps<T = any> {
+      type?: 'tabs' | 'menu'
+      value?: Array<T>
+      options?: Array<IOption<T>>
+      title?: Array<React.ReactNode>
+      onChange?: (value: Array<IItem>) => any
+      loadMore?: () => Promise<ILoadMoreResolvedItem<T>>
+      changeOnSelect?: boolean
+      placeholder?: string
+      prefix?: string
+      className?: string
+      popClassName?: string
+      displayText?: (value: Array<IItem>) => React.ReactNode
+      expandTrigger?: 'click' | 'hover'
+    }
+
   }
 
-  interface ICascaderOption extends ICascaderItem {
-    children?: ICascaderOption[]
-  }
+  class Cascader<T = any> extends React.Component<Cascader.IProps<T>, any> { }
 
-  interface ICascaderLoadMoreResolvedItem extends ICascaderItem {
-    isLeaf: boolean
-  }
-
-  interface ICascaderProps {
-    type?: 'tabs' | 'menu'
-    value?: Array<any>
-    options?: Array<ICascaderOption>
-    title?: Array<React.ReactNode>
-    onChange?: (value: Array<ICascaderItem>) => any
-    loadMore?: () => Promise<ICascaderLoadMoreResolvedItem>
-    changeOnSelect?: boolean
-    placeholder?: string
-    prefix?: string
-    className?: string
-    popClassName?: string
-    displayText?: (value: Array<ICascaderItem>) => React.ReactNode
-    expandTrigger?: 'click' | 'hover'
-  }
-
-  export default class Cascader extends React.Component<ICascaderProps, any> {}
+  export default Cascader;
 }
