4b34157ddbdab1a0fb6f6f4d40512b5cc2d2ab5e
Charles Lee <zombiefml@gmail.com>
2018-11-19 15:21:29 +0800

    Refactor: extract local functions to class methods

--- packages/zent/src/progress/Progress.js
+++ packages/zent/src/progress/Progress.js
@@ -30,26 +30,157 @@ export default class Progress extends PureComponent {
     strokeWidth: 10,
   };
 
-  render() {
+  getCurrentColor() {
+    const {
+      percent,
+      status,
+      normalColor,
+      exceptionColor,
+      successColor,
+    } = this.props;
+
+    if (percent < 100 && status === 'exception') {
+      return exceptionColor || normalColor;
+    }
+    if (percent >= 100) {
+      return successColor;
+    }
+    return normalColor;
+  }
+
+  renderInfo() {
+    const { type, percent, status, format } = this.props;
+
+    if (format) {
+      return format(percent);
+    }
+
+    if (percent >= 100) {
+      return (
+        <Icon
+          type={type === 'circle' ? 'check' : 'check-circle'}
+          style={{
+            color: this.getCurrentColor(),
+          }}
+        />
+      );
+    } else if (status === 'exception') {
+      return (
+        <Icon
+          type={type === 'circle' ? 'close' : 'close-circle'}
+          style={{
+            color: this.getCurrentColor(),
+          }}
+        />
+      );
+    }
+
+    return `${percent}%`;
+  }
+
+  renderProgress() {
     const {
       type,
       percent,
       showInfo,
       status,
-      className,
       prefix,
-      format,
       strokeWidth,
       width,
-      style,
-      normalColor,
-      exceptionColor,
-      successColor,
       bgColor,
     } = this.props;
-
     const progressWidth = width || (type === 'circle' ? 132 : 580);
+    const statusCls = cx({
+      [`${prefix}-progress-inprogress`]:
+        percent < 100 && status !== 'exception',
+      [`${prefix}-progress-exception`]: percent < 100 && status === 'exception',
+      [`${prefix}-progress-success`]: percent >= 100,
+    });
+    let mid, diameter, radius, circumference;
 
+    switch (type) {
+      case 'circle':
+        mid = progressWidth / 2;
+        diameter = progressWidth - strokeWidth;
+        radius = diameter / 2;
+        circumference = diameter * Math.PI;
+
+        return (
+          <div
+            className={statusCls}
+            style={{
+              width: progressWidth,
+              height: progressWidth,
+            }}
+          >
+            <div
+              className={`${prefix}-progress-wrapper`}
+              style={{
+                borderRadius: progressWidth,
+                borderWidth: strokeWidth,
+                borderColor: bgColor,
+              }}
+            />
+            <svg className={`${prefix}-progress-inner`}>
+              <circle
+                className={`${prefix}-progress-inner-path`}
+                cx={mid}
+                cy={mid}
+                r={radius}
+                style={{
+                  stroke: this.getCurrentColor(),
+                  strokeWidth,
+                  strokeDasharray: circumference,
+                  strokeDashoffset: circumference * (100 - percent) / 100,
+                }}
+              />
+            </svg>
+            {showInfo ? (
+              <div
+                className={`${prefix}-progress-info`}
+                style={{ lineHeight: `${progressWidth}px` }}
+              >
+                {this.renderInfo()}
+              </div>
+            ) : null}
+          </div>
+        );
+
+      case 'line': /* fall through */
+      default:
+        return (
+          <div className={statusCls}>
+            <div
+              className={`${prefix}-progress-wrapper`}
+              style={{
+                background: bgColor,
+                width: progressWidth,
+                height: strokeWidth,
+                borderRadius: strokeWidth,
+              }}
+            >
+              <div
+                className={`${prefix}-progress-inner`}
+                style={{
+                  background: this.getCurrentColor(),
+                  width: `${percent}%`,
+                  height: strokeWidth,
+                  borderRadius: strokeWidth,
+                }}
+              />
+            </div>
+            {showInfo ? (
+              <div className={`${prefix}-progress-info`}>
+                {this.renderInfo()}
+              </div>
+            ) : null}
+          </div>
+        );
+    }
+  }
+
+  render() {
+    const { type, className, prefix, style } = this.props;
     const containerCls = cx(
       `${prefix}-progress`,
       `${prefix}-progress-${type}`,
@@ -58,139 +189,9 @@ export default class Progress extends PureComponent {
       }
     );
 
-    const statusCls = cx({
-      [`${prefix}-progress-inprogress`]:
-        percent < 100 && status !== 'exception',
-      [`${prefix}-progress-exception`]: percent < 100 && status === 'exception',
-      [`${prefix}-progress-success`]: percent >= 100,
-    });
-
-    const getCurrentColor = () => {
-      if (percent < 100 && status === 'exception') {
-        return exceptionColor || normalColor;
-      }
-      if (percent >= 100) {
-        return successColor;
-      }
-      return normalColor;
-    };
-
-    const renderInfoCont = () => {
-      let infoCont;
-      if (format) {
-        infoCont = format(percent);
-      } else {
-        infoCont = `${percent}%`;
-        if (percent >= 100) {
-          infoCont = (
-            <Icon
-              type={type === 'circle' ? 'check' : 'check-circle'}
-              style={{
-                color: getCurrentColor(),
-              }}
-            />
-          );
-        } else if (status === 'exception') {
-          infoCont = (
-            <Icon
-              type={type === 'circle' ? 'close' : 'close-circle'}
-              style={{
-                color: getCurrentColor(),
-              }}
-            />
-          );
-        }
-      }
-      return infoCont;
-    };
-
-    const renderProgressCont = () => {
-      let progressCont;
-      switch (type) {
-        case 'circle':
-          progressCont = (
-            <div
-              className={statusCls}
-              style={{
-                width: progressWidth,
-                height: progressWidth,
-              }}
-            >
-              <div
-                className={`${prefix}-progress-wrapper`}
-                style={{
-                  borderRadius: progressWidth,
-                  borderWidth: strokeWidth,
-                  borderColor: bgColor,
-                }}
-              />
-              <svg className={`${prefix}-progress-inner`}>
-                <circle
-                  className={`${prefix}-progress-inner-path`}
-                  cx={progressWidth / 2}
-                  cy={progressWidth / 2}
-                  r={(progressWidth - strokeWidth) / 2}
-                  style={{
-                    stroke: getCurrentColor(),
-                    strokeWidth,
-                    strokeDasharray: Math.PI * (progressWidth - strokeWidth),
-                    strokeDashoffset:
-                      Math.PI *
-                      (progressWidth - strokeWidth) *
-                      (100 - percent) /
-                      100,
-                  }}
-                />
-              </svg>
-              {showInfo ? (
-                <div
-                  className={`${prefix}-progress-info`}
-                  style={{ lineHeight: `${progressWidth}px` }}
-                >
-                  {renderInfoCont()}
-                </div>
-              ) : null}
-            </div>
-          );
-          break;
-        case 'line':
-        default:
-          progressCont = (
-            <div className={statusCls}>
-              <div
-                className={`${prefix}-progress-wrapper`}
-                style={{
-                  background: bgColor,
-                  width: progressWidth,
-                  height: strokeWidth,
-                  borderRadius: strokeWidth,
-                }}
-              >
-                <div
-                  className={`${prefix}-progress-inner`}
-                  style={{
-                    background: getCurrentColor(),
-                    width: `${percent}%`,
-                    height: strokeWidth,
-                    borderRadius: strokeWidth,
-                  }}
-                />
-              </div>
-              {showInfo ? (
-                <div className={`${prefix}-progress-info`}>
-                  {renderInfoCont()}
-                </div>
-              ) : null}
-            </div>
-          );
-          break;
-      }
-      return progressCont;
-    };
-
     return (
       <div className={containerCls} style={style}>
-        {renderProgressCont()}
+        {this.renderProgress()}
       </div>
     );
   }
