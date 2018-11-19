1ea65ec515b0edf12511729e1cdd895348f29814
fancy <lucasicarus@hotmail.com>
2018-11-19 17:26:44 +0800

    [feature]: 适配新的 Select 样式

--- packages/zent/src/select/trigger/BaseTrigger.js
+++ packages/zent/src/select/trigger/BaseTrigger.js
@@ -1,17 +1,19 @@
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
+import cx from 'classnames';
 
 import { I18nReceiver as Receiver } from 'i18n';
 import { Select as I18nDefault } from 'i18n/default';
 
 class SelectTrigger extends PureComponent {
   render() {
-    const { prefixCls, onClick } = this.props;
+    const { prefixCls, onClick, visible } = this.props;
+    const rootClass = cx(`${prefixCls}-text`, { visible });
 
     return (
       <Receiver componentName="Select" defaultI18n={I18nDefault}>
         {i18n => (
-          <div className={`${prefixCls}-text`} onClick={onClick}>
+          <div className={rootClass} onClick={onClick}>
             {this.props.text || (
               <span className={`${prefixCls}-placeholder`}>
                 {this.props.placeholder || i18n.input}
