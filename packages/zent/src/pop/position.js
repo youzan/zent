d679deb4e5983d5840b70bb1c35feab6efacefc5
Charles Lee <cpylua@users.noreply.github.com>
2019-01-04 16:59:29 +0800

    feat: suppport function is Pop.position (#986)

--- packages/zent/src/pop/position.js
+++ packages/zent/src/pop/position.js
@@ -1,5 +1,6 @@
 import Popover from 'popover';
 import capitalize from 'lodash/capitalize';
+import isFunction from 'lodash/isFunction';
 
 const { Position } = Popover;
 
@@ -135,6 +136,10 @@ const CenterArrowPosition = {
 };
 
 export default function getPosition(position, centerArrow) {
+  if (isFunction(position)) {
+    return position;
+  }
+
   let positionName = position
     .split('-')
     .map(s => capitalize(s))
