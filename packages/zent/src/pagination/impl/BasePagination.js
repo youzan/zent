9c90c78eb63b605300a5ecf0d27e26bbaaf215e3
Charles Lee <cpylua@users.noreply.github.com>
2019-01-15 14:55:09 +0800

    [new feature] Pagination: New pagination (#994)
    * feat: new pagination component
    
    * fix: new typings definition

---
+++ packages/zent/src/pagination/impl/BasePagination.js
@@ -0,0 +1,51 @@
+import { Component } from 'react';
+
+export default class BasePagination extends Component {
+  shouldUpdateLayout(props, nextProps) {
+    const { current, total, pageSize } = nextProps;
+
+    return (
+      current !== props.current ||
+      total !== props.total ||
+      pageSize !== props.pageSize
+    );
+  }
+
+  getLayoutOptions(props) {
+    const { current, total, pageSize } = props;
+
+    return {
+      current,
+      total,
+      pageSize,
+    };
+  }
+
+  onPageChange = page => {
+    const { current, total, pageSize, onChange } = this.props;
+    page = Math.max(1, page);
+    page = Math.min(page, this.getTotalPages(total, pageSize));
+
+    if (page !== current) {
+      onChange({
+        current: page,
+        pageSize,
+      });
+    }
+  };
+
+  onPageSizeChange = pageSize => {
+    const { total, current } = this.props;
+    const maxPageNumber = this.getTotalPages(total, pageSize);
+    const options = {
+      pageSize,
+      current: Math.min(current, maxPageNumber),
+    };
+
+    this.props.onChange(options);
+  };
+
+  getTotalPages(total, pageSize) {
+    return Math.ceil(total / pageSize);
+  }
+}
