f8f516260160d03b2a3f428319ae99a79b1229aa
Charles Lee <zombiefml@gmail.com>
2019-01-15 14:54:28 +0800

    fix: new typings definition

--- packages/zent/typings/libs/Pagination.d.ts
+++ packages/zent/typings/libs/Pagination.d.ts
@@ -1,16 +1,33 @@
 /// <reference types="react" />
 
 declare module 'zent/lib/pagination' {
-  interface IPaginationProps {
-    current: number
-    totalItem: number
-    pageSize?: number
-    onPageSizeChange?: (pageSize: number) => any
-    maxPageToShow?: number
-    onChange?: (value: number) => void
-    className?: string
-    prefix?: string
+  interface CompoundPageSizeOption {
+    text: string;
+    value: number;
   }
 
-  export default class Pagination extends React.Component<IPaginationProps, any> { }
+  type PageSizeOption = number | CompoundPageSizeOption;
+
+  interface PaginationOnChangeParameter {
+    current: number;
+    pageSize: number;
+  }
+
+  interface IPaginationProps {
+    type?: 'normal' | 'lite' | 'mini';
+    current?: number;
+    total?: number;
+    pageSize?: number;
+    pageSizeOptions?: PageSizeOption[];
+    onChange: (arg: PaginationOnChangeParameter) => any;
+    showQuickJumper?: boolean;
+    showSizeChanger?: boolean;
+    buttonBordered?: boolean;
+    className?: string;
+  }
+
+  export default class Pagination extends React.Component<
+    IPaginationProps,
+    any
+  > {}
 }
