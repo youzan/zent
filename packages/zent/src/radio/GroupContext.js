5083fcf3b8eeb08aa24e4b90756ed4cee304c57a
まきちゃん~ <intellild@outlook.com>
2019-01-30 11:14:41 +0800

    migrate Radio

--- packages/zent/src/radio/GroupContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default React.createContext({
-  value: [],
-  disabled: false,
-  readOnly: false,
-  isValueEqual: (a, b) => a === b,
-  onRadioChange: null,
-});
---
+++ packages/zent/src/radio/GroupContext.tsx
@@ -0,0 +1,19 @@
+import { createContext } from 'react';
+
+import { IRadioEvent } from './Radio';
+
+export interface IRadioContext {
+  value: unknown;
+  isValueEqual(a: unknown, b: unknown): boolean;
+  disabled: boolean;
+  readOnly: boolean;
+  onRadioChange: ((e: IRadioEvent) => void) | null;
+}
+
+export default createContext<IRadioContext>({
+  value: [],
+  disabled: false,
+  readOnly: false,
+  isValueEqual: (a: unknown, b: unknown) => a === b,
+  onRadioChange: null,
+});
