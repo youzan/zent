032f43693d33ac606d3f085f986ea9b219f664a7
まきちゃん~ <intellild@outlook.com>
2019-01-30 16:47:02 +0800

    migrate Cascader

--- packages/zent/src/popover/withPopover.tsx
+++ packages/zent/src/popover/withPopover.tsx
@@ -1,29 +1,32 @@
 import * as React from 'react';
 import { Component } from 'react';
+import { Assign } from 'utility-types';
 
 import PopoverContext, { IPopoverContext } from './PopoverContext';
+import Popover from './Popover';
 
 /**
  * A high order component to expose imperative APIs for popover.
  *
  * Adds a popover prop to component.
  */
-export const exposePopover = (propName: string) => (
-  Base: React.ComponentType<any>
-) => {
-  return class ExposePopover extends Component {
-    static contextType = PopoverContext;
+export function exposePopover<N extends string>(propName: N) {
+  return function<T extends {}>(Base: React.ComponentType<T>) {
+    return class ExposePopover extends Component<T> {
+      render() {
+        const { _zentPopover: popover } =
+          this.context || ({} as IPopoverContext);
+        const { registerDescendant, unregisterDescendant, ...others } = popover;
+        const context = {
+          [propName]: others,
+        };
 
-    render() {
-      const { _zentPopover: popover } = this.context || ({} as IPopoverContext);
-      const { registerDescendant, unregisterDescendant, ...others } = popover;
-      const context = {
-        [propName]: others,
-      };
-
-      return <Base {...this.props} {...context} />;
-    }
+        return <Base {...this.props} {...context} />;
+      }
+    };
   };
-};
+}
 
-export default exposePopover('popover');
+export default exposePopover('popover') as <T extends {}>(
+  Comp: React.ComponentType<T & { popover: Popover }>
+) => React.ComponentType<T>;
