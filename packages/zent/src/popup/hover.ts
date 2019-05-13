ddfa34d0d4152567185ed618e07e213061cfc2da
intellild <intellild@outlook.com>
2019-05-13 10:11:04 +0800

    initial hover logic

--- packages/zent/src/popup/hover.ts
+++ packages/zent/src/popup/hover.ts
@@ -1,6 +1,78 @@
 import * as React from 'react';
-import { usePopup } from './Popup';
+import { usePopup, Popup } from './Popup';
 import { IPositionCalculator } from './position';
+import { IDot, isLineIntersectRect, ILine, IRect } from './utils';
+
+const SPEED_THRESHOLD = 0;
+
+function boundingClientRectToIRect({
+  left,
+  top,
+  width,
+  height,
+}: ClientRect | DOMRect): IRect {
+  return {
+    a: {
+      x: left,
+      y: top,
+    },
+    b: {
+      x: left + width,
+      y: top,
+    },
+    c: {
+      x: left,
+      y: top + height,
+    },
+    d: {
+      x: left + width,
+      y: top + height,
+    },
+  };
+}
+
+function windowMouseMove<Anchor extends Element, Content extends Element>(
+  positionRef: React.MutableRefObject<IDot>,
+  isMouseIn: React.RefObject<boolean>,
+  timeRef: React.MutableRefObject<number>,
+  e: MouseEvent,
+  popup: Popup<Anchor, Content>
+) {
+  const prev = positionRef.current;
+  const x = e.screenX;
+  const y = e.screenY;
+  const pos = {
+    x,
+    y,
+  };
+  positionRef.current = pos;
+  const now = Date.now();
+  const prevTime = timeRef.current;
+  timeRef.current = now;
+  const anchor = popup.anchorRef.current;
+  const content = popup.contentRef.current;
+  if (isMouseIn.current || !prev || !content || !anchor || !popup.visible) {
+    return;
+  }
+  const duration = now - prevTime;
+  const distance = Math.sqrt(Math.pow(x - prev.x, 2) + Math.pow(y - prev.y, 2));
+  const speed = distance / duration;
+  const line: ILine = {
+    a: prev,
+    b: pos,
+  };
+  const contentRect = boundingClientRectToIRect(
+    content.getBoundingClientRect()
+  );
+  const anchorRect = boundingClientRectToIRect(anchor.getBoundingClientRect());
+  const isTowardsAnchor = isLineIntersectRect(line, anchorRect);
+  const isTowardsContent = isLineIntersectRect(line, contentRect);
+  console.log(isTowardsAnchor, isTowardsContent, line, anchorRect, contentRect);
+  if (speed > SPEED_THRESHOLD && (isTowardsAnchor || isTowardsContent)) {
+    return;
+  }
+  popup.setVisible(false);
+}
 
 export interface IHoverProps<E extends Element> {
   onMouseEnter: React.MouseEventHandler<E>;
@@ -17,6 +89,9 @@ export function useHoverPopup<
   const popup = usePopup<Anchor, Content>(pos);
   const propsRef = React.useRef(props);
   propsRef.current = props;
+  const isMouseIn = React.useRef(false);
+  const positionRef = React.useRef<IDot | null>(null);
+  const timeRef = React.useRef(0);
   const anchorProps = React.useMemo<IHoverProps<Anchor>>(
     () => ({
       ref: popup.anchorRef,
@@ -26,6 +101,7 @@ export function useHoverPopup<
           const { onMouseEnter } = props;
           onMouseEnter && onMouseEnter(e);
         }
+        isMouseIn.current = true;
         popup.setVisible(true);
       },
       onMouseLeave(e) {
@@ -34,10 +110,19 @@ export function useHoverPopup<
           const { onMouseLeave } = props;
           onMouseLeave && onMouseLeave(e);
         }
-        popup.setVisible(false);
+        isMouseIn.current = false;
       },
     }),
     [popup]
   );
+  React.useEffect(() => {
+    function onMouseMove(e: MouseEvent) {
+      windowMouseMove(positionRef, isMouseIn, timeRef, e, popup);
+    }
+    window.addEventListener('mousemove', onMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', onMouseMove);
+    };
+  }, []);
   return [anchorProps, popup];
 }
