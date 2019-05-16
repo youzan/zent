12b5fac35487bf9331629f09af1dafa90c74cf3a
intellild <intellild@outlook.com>
2019-05-16 20:42:59 +0800

    refactor focus and click trigger

--- packages/zent/src/popover/trigger/ClickTrigger.ts
+++ packages/zent/src/popover/trigger/ClickTrigger.ts
@@ -1,64 +1,83 @@
+import { findDOMNode } from 'react-dom';
+import { isElement } from 'react-is';
 import Trigger, { IPopoverTriggerProps } from './Trigger';
+import PopoverContext, { IPopoverContext, getContext } from '../PopoverContext';
+import { RefObject } from 'react';
+import { IPortalImperativeHandlers } from '../../portal';
 
-export interface IPopoverClickTriggerProps extends IPopoverTriggerProps {
+export interface IPopoverClickTriggerChildProps {
+  onClick: React.MouseEventHandler;
+}
+
+export interface IPopoverClickTriggerProps<
+  P extends IPopoverClickTriggerChildProps
+> extends IPopoverTriggerProps<P> {
   autoClose?: boolean;
 }
 
+function isOutside(
+  el: Element,
+  portalRef: RefObject<IPortalImperativeHandlers>,
+  anchor: null | Element | Text
+) {
+  if (!anchor || !(anchor instanceof Element)) {
+    return false;
+  }
+  if (anchor.contains(el)) {
+    return false;
+  }
+  const portal = portalRef.current;
+  if (!portal) {
+    return true;
+  }
+  if (portal.contains(el)) {
+    return false;
+  }
+  return true;
+}
+
 export default class PopoverClickTrigger<
-  P extends IPopoverClickTriggerProps = IPopoverClickTriggerProps
-> extends Trigger<P> {
+  P extends IPopoverClickTriggerChildProps
+> extends Trigger<
+  IPopoverClickTriggerChildProps,
+  IPopoverClickTriggerProps<P>
+> {
   static defaultProps = {
     autoClose: true,
   };
 
-  onClickOutside = evt => {
-    // Optimization: skip checking if popover is hidden
-    const { contentVisible } = this.props;
-    if (!contentVisible) {
+  static contextType = PopoverContext;
+  context!: IPopoverContext;
+
+  protected childProps: IPopoverClickTriggerChildProps = {
+    onClick: e => {
+      const { children } = this.props;
+      const { popover, visible } = getContext(this);
+      if (!visible) {
+        popover.open();
+      }
+      if (isElement(children)) {
+        const { onClick } = children.props;
+        onClick && onClick(e);
+      }
+    },
+  };
+
+  onClick = (e: MouseEvent) => {
+    if (!(e.target instanceof Element) || !this.props.autoClose) {
       return;
     }
-
-    const { target } = evt;
-    if (this.props.isOutsideStacked(target)) {
-      this.props.close();
+    const { portalRef, popover, visible } = getContext(this);
+    if (visible && isOutside(e.target, portalRef, findDOMNode(this))) {
+      popover.close();
     }
   };
 
-  getTriggerProps(child) {
-    return {
-      onClick: evt => {
-        this.props.open();
-        this.triggerEvent(child, 'onClick', evt);
-      },
-    };
-  }
-
-  bindEventHandler(props?: IPopoverClickTriggerProps) {
-    const { contentVisible, autoClose } = props || this.props;
-
-    // bind global events only when popover is visible
-    if (autoClose && contentVisible) {
-      return window.addEventListener('click', this.onClickOutside, true);
-    }
-
-    // Ensure handler is removed even if autoClose is false
-    if (!contentVisible) {
-      return window.removeEventListener('click', this.onClickOutside, true);
-    }
+  componentDidMount() {
+    window.addEventListener('click', this.onClick);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('click', this.onClickOutside, true);
-  }
-
-  componentDidMount() {
-    this.bindEventHandler();
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { contentVisible } = nextProps;
-    if (contentVisible !== this.props.contentVisible) {
-      this.bindEventHandler(nextProps);
-    }
+    window.removeEventListener('click', this.onClick, true);
   }
 }
--- packages/zent/src/popover/trigger/FocusTrigger.ts
+++ packages/zent/src/popover/trigger/FocusTrigger.ts
@@ -1,21 +1,36 @@
 import Trigger, { IPopoverTriggerProps } from './Trigger';
+import { getContext } from '../PopoverContext';
+import { isElement } from 'react-is';
 
-export interface IFocusTriggerProps extends IPopoverTriggerProps {}
+export interface IFocusTriggerChildProps {
+  onFocus: React.FocusEventHandler;
+  onBlur: React.FocusEventHandler;
+}
+
+export interface IFocusTriggerProps<P extends IFocusTriggerChildProps>
+  extends IPopoverTriggerProps<P> {}
 
 export default class FocusTrigger<
-  P extends IFocusTriggerProps = IFocusTriggerProps
-> extends Trigger<P> {
-  getTriggerProps(child) {
-    return {
-      onFocus: evt => {
-        this.props.open();
-        this.triggerEvent(child, 'onFocus', evt);
-      },
-
-      onBlur: evt => {
-        this.props.close();
-        this.triggerEvent(child, 'onBlur', evt);
-      },
-    };
-  }
+  P extends IFocusTriggerChildProps = IFocusTriggerChildProps
+> extends Trigger<IFocusTriggerChildProps, IFocusTriggerProps<P>> {
+  protected triggerProps: IFocusTriggerChildProps = {
+    onFocus: e => {
+      const { children } = this.props;
+      const { popover } = getContext(this);
+      popover.open();
+      if (isElement(children)) {
+        const { onFocus } = children.props;
+        onFocus && onFocus(e);
+      }
+    },
+    onBlur: e => {
+      const { children } = this.props;
+      const { popover } = getContext(this);
+      popover.close();
+      if (isElement(children)) {
+        const { onBlur } = children.props;
+        onBlur && onBlur(e);
+      }
+    },
+  };
 }
