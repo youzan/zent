eded413f95c0fa2a67848e3f7351e88a695e697c
intellild <intellild@outlook.com>
2019-05-24 10:43:10 +0800

    refactor patch process to seperate functions

--- packages/zent/src/portal/Portal.tsx
+++ packages/zent/src/portal/Portal.tsx
@@ -41,6 +41,39 @@ interface IPatchMeta {
 
 const patched = new Map<HTMLElement, IPatchMeta>();
 
+function patchElement(parent: HTMLElement) {
+  const meta = patched.get(parent);
+  if (meta) {
+    meta.count += 1;
+  } else {
+    const { overflowY, paddingRight } = parent.style;
+    const originalPadding = getComputedStyle(parent).paddingRight;
+    const newPadding = parseFloat(originalPadding || '0') + SCROLLBAR_WIDTH;
+    parent.style.overflowY = 'hidden';
+    parent.style.paddingRight = `${newPadding}px`;
+    const newMeta: IPatchMeta = {
+      count: 1,
+      overflowY,
+      paddingRight,
+    };
+    patched.set(parent, newMeta);
+  }
+}
+
+function restoreElement(parent: HTMLElement) {
+  const meta = patched.get(parent);
+  if (!meta) {
+    throw new Error('This looks like a bug of zent, please file an issue');
+  }
+  if (meta.count === 1) {
+    patched.delete(parent);
+    parent.style.overflowY = meta.overflowY;
+    parent.style.paddingRight = meta.paddingRight;
+  } else {
+    meta.count -= 1;
+  }
+}
+
 export interface IPortalProps extends Partial<IPurePortalProps> {
   visible?: boolean;
   layer?: string;
@@ -148,37 +181,8 @@ export const Portal = forwardRef<IPortalImperativeHandlers, IPortalProps>(
       ) {
         return noop;
       }
-      const meta = patched.get(parent);
-      if (meta) {
-        meta.count += 1;
-      } else {
-        const { overflowY, paddingRight } = parent.style;
-        const originalPadding = getComputedStyle(parent).paddingRight;
-        const newPadding = parseFloat(originalPadding || '0') + SCROLLBAR_WIDTH;
-        parent.style.overflowY = 'hidden';
-        parent.style.paddingRight = `${newPadding}px`;
-        const newMeta: IPatchMeta = {
-          count: 1,
-          overflowY,
-          paddingRight,
-        };
-        patched.set(parent, newMeta);
-      }
-      return () => {
-        const meta = patched.get(parent);
-        if (!meta) {
-          throw new Error(
-            'This looks like a bug of zent, please file an issue'
-          );
-        }
-        if (meta.count === 1) {
-          patched.delete(parent);
-          parent.style.overflowY = meta.overflowY;
-          parent.style.paddingRight = meta.paddingRight;
-        } else {
-          meta.count -= 1;
-        }
-      };
+      patchElement(parent);
+      return () => restoreElement(parent);
     }, [parent, visible, blockPageScroll]);
 
     useLayoutEffect(() => {
