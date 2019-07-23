18c5758934c04a5caa7221b389e4f0e9870d612f
Charles Lee <zombiefml@gmail.com>
2019-07-23 10:13:48 +0800

    Add thread-loader for loading markdown files

--- site/webpack/webpack.config.js
+++ site/webpack/webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const Fiber = require('fibers');
 const sass = require('sass');
+const os = require('os');
 const { join, resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -83,7 +84,41 @@ module.exports = {
       {
         test: /\.md$/,
         use: [
-          'cache-loader',
+          {
+            loader: 'thread-loader',
+            // loaders with equal options will share worker pools
+            options: {
+              // the number of spawned workers, defaults to (number of cpus - 1) or
+              // fallback to 1 when require('os').cpus() is undefined
+              workers: os.cpus() - 2,
+
+              // number of jobs a worker processes in parallel
+              // defaults to 20
+              workerParallelJobs: 10,
+
+              // additional node.js arguments
+              workerNodeArgs: ['--max-old-space-size=1024'],
+
+              // Allow to respawn a dead worker pool
+              // respawning slows down the entire compilation
+              // and should be set to false for development
+              poolRespawn: !DEV,
+
+              // timeout for killing the worker processes when idle
+              // defaults to 500 (ms)
+              // can be set to Infinity for watching builds to keep workers alive
+              poolTimeout: DEV ? Infinity : 500,
+
+              // number of jobs the poll distributes to the workers
+              // defaults to 200
+              // decrease of less efficient but more fair distribution
+              poolParallelJobs: 50,
+
+              // name of the pool
+              // can be used to create different pools with elsewise identical options
+              name: 'md-pool',
+            },
+          },
           'babel-loader',
           {
             loader: 'react-markdown-doc-loader',
