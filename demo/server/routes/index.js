d34112c2102c5847c178df296735c8f34725ce93
Charles Lee <cpylua@users.noreply.github.com>
2019-09-15 12:10:52 +0800

    Remove demos [ci skip] (#1171)

--- demo/server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-router.use('/shop', require('./shop'));
-router.use('/order', require('./order'));
-
-module.exports = router;
