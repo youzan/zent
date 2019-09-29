a7222a90301b57009fff0418445dcf418208af24
intellild <intellild@outlook.com>
2019-09-29 20:47:54 +0800

    export container from portal

--- packages/zent/src/portal/Portal.tsx
+++ packages/zent/src/portal/Portal.tsx
@@ -91,6 +91,7 @@ export interface IPortalProps extends Partial<IPurePortalProps> {
 export interface IPortalImperativeHandlers {
   contains(node: Node): boolean;
   purePortalRef: React.RefObject<PurePortal | undefined>;
+  container: HTMLElement;
 }
 
 export const Portal = forwardRef<IPortalImperativeHandlers, IPortalProps>(
@@ -126,8 +127,9 @@ export const Portal = forwardRef<IPortalImperativeHandlers, IPortalProps>(
           return purePortal.contains(node);
         },
         purePortalRef,
+        container: node,
       }),
-      []
+      [node]
     );
 
     useLayoutEffect(() => {
