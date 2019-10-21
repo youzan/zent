b95adfcfe0f79f7e1cc4337ce360491bcb2f7162
zhangshikai <zhangshikai@youzan.com>
2019-10-21 16:47:37 +0800

    fix: 修复 tabs 的 panel 在切换选项时重复触发生命周期的问题

--- packages/zent/src/tabs/components/TabPanel.tsx
+++ packages/zent/src/tabs/components/TabPanel.tsx
@@ -4,14 +4,11 @@ import { ITabPanelProps } from '../types';
 
 function TabPanel<Id>(props: React.PropsWithChildren<ITabPanelProps<Id>>) {
   const { actived, className, children } = props;
-
-  if (!actived) {
-    return null;
-  }
+  const displayStyle: React.CSSProperties = actived ? {} : { display: 'none' };
 
   const panelCls = cn('zent-tabs-panel', className);
   return (
-    <div role="tabpanel" className={panelCls}>
+    <div role="tabpanel" style={displayStyle} className={panelCls}>
       {children}
     </div>
   );
