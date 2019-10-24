06b1dba26ae30f46fc8c35e7f3316022a97a56b1
KainStar <zhangshikai@youzan.com>
2019-10-24 11:31:38 +0800

    [bugfix] Tabs: 添加 Panel 的不同渲染行为模式 (#1210)
    * fix: 修复 tabs 的 panel 在切换选项时重复触发生命周期的问题
    
    * fix: 添加 forceRenderPanel 属性用于控制 tabs 的渲染行为
    
    * fix: 修改 tabs 的 panel 默认渲染行为

--- packages/zent/src/tabs/components/TabPanel.tsx
+++ packages/zent/src/tabs/components/TabPanel.tsx
@@ -3,15 +3,18 @@ import cn from 'classnames';
 import { ITabPanelProps } from '../types';
 
 function TabPanel<Id>(props: React.PropsWithChildren<ITabPanelProps<Id>>) {
-  const { actived, className, children } = props;
+  const { actived, className, unmountOnHide, children } = props;
 
-  if (!actived) {
+  // 启用 unmountOnHide 时，非 active 元素不渲染，直接返回 null
+  if (!actived && unmountOnHide) {
     return null;
   }
 
+  const displayStyle: React.CSSProperties = actived ? {} : { display: 'none' };
+
   const panelCls = cn('zent-tabs-panel', className);
   return (
-    <div role="tabpanel" className={panelCls}>
+    <div role="tabpanel" style={displayStyle} className={panelCls}>
       {children}
     </div>
   );
