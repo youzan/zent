3ce679fb5aab0616cb21019272fe63bca6e3510d
Charles Lee <cpylua@users.noreply.github.com>
2019-11-12 15:06:05 +0800

    Upgrade to date-fns@2 (#1242)

--- packages/zent/src/datetimepicker/WeekPicker.tsx
+++ packages/zent/src/datetimepicker/WeekPicker.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { PureComponent } from 'react';
 import cx from 'classnames';
 import { Omit } from 'utility-types';
-const startOfWeek = require('date-fns/start_of_week');
-const endOfWeek = require('date-fns/end_of_week');
-const addDays = require('date-fns/add_days');
-const subDays = require('date-fns/sub_days');
-const differenceInCalendarDays = require('date-fns/difference_in_calendar_days');
+import startOfWeek from 'date-fns/startOfWeek';
+import endOfWeek from 'date-fns/endOfWeek';
+import addDays from 'date-fns/addDays';
+import subDays from 'date-fns/subDays';
+import differenceInCalendarDays from 'date-fns/differenceInCalendarDays';
 
 import Input from '../input';
 import Popover from '../popover';
@@ -29,8 +29,8 @@ import { DatePickers } from './common/types';
 
 function getSelectedWeek(
   val,
-  start = 1,
-  isDisabled,
+  start: 0 | 1 | 2 | 3 | 4 | 5 | 6,
+  isDisabled: (val: Date, props: IWeekPickerProps) => boolean,
   props: IWeekPickerProps
 ): [Date?, Date?] {
   let weekStart = startOfWeek(val, {
@@ -130,7 +130,7 @@ function extractStateFromProps(props: IWeekPickerProps) {
 
 export interface IWeekPickerProps
   extends Omit<DatePickers.ICommonProps<DatePickers.RangeValue>, 'onClick'> {
-  startDay?: number;
+  startDay?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
   onBeforeClear?: () => boolean;
   onClick?: (val: DatePickers.RangeValue, type?: DatePickers.RangeType) => void;
   isFooterVisible?: boolean;
