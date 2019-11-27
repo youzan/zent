6f8c040c330ec74910d1cfbfb377ee7937d834dc
Charles Lee <zombiefml@gmail.com>
2019-11-27 10:56:00 +0800

    Update RELEASE
    Add missing version command
    
    fix publish script
    
    Fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,9 +4,10 @@ set -e
 
 basepath=$(dirname $0)
 
+# bump version first, build relies on it
+yarn workspace "$1" version
+
 # 重新bootstrap，以防有人改了依赖
 yarn bootstrap "$1"
 
-# bump version first, build relies on it
-yarn workspace "$1" version
 yarn workspace "$1" publish --non-interactive "${@:2}"
