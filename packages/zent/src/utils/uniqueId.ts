c14ecaf9eb1dcb4239020ea9c0ef74e1e1f982a1
Charles Lee <zombiefml@gmail.com>
2019-12-02 17:06:48 +0800

    Add some randomness to uniqueId

--- packages/zent/src/utils/uniqueId.ts
+++ packages/zent/src/utils/uniqueId.ts
@@ -1,5 +1,7 @@
 let id = 0;
-const PREFIX = __ZENT_VERSION__.replace(/[^0-9a-z]/gi, 'x');
+const PREFIX =
+  __ZENT_VERSION__.replace(/[^0-9a-z]/gi, 'x') +
+  Math.ceil(Math.random() * 10000).toString();
 
 export default function uniqueId(prefix?: string): string {
   const nextId = `${PREFIX}${++id}`;
