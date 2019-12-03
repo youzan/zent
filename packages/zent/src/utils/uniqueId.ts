2d5d7f6d4c35fcacb3fdb3b8c5b5fb0b4f8b8981
Charles Lee <zombiefml@gmail.com>
2019-12-03 10:34:45 +0800

    uniqueId starts with letter

--- packages/zent/src/utils/uniqueId.ts
+++ packages/zent/src/utils/uniqueId.ts
@@ -1,7 +1,8 @@
 let id = 0;
 const PREFIX =
+  'v' +
   __ZENT_VERSION__.replace(/[^0-9a-z]/gi, 'x') +
-  Math.ceil(Math.random() * 10000).toString();
+  Math.ceil(Math.random() * 8999 + 1000).toString();
 
 export default function uniqueId(prefix?: string): string {
   const nextId = `${PREFIX}${++id}`;
