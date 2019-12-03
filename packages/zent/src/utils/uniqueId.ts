c701995a8ada69b08b745901d8bff3817a72b0a7
Charles Lee <cpylua@users.noreply.github.com>
2019-12-03 13:05:36 +0800

    Add some randomness to uniqueId (#1275)
    * Add some randomness to uniqueId
    
    * uniqueId starts with letter

--- packages/zent/src/utils/uniqueId.ts
+++ packages/zent/src/utils/uniqueId.ts
@@ -1,5 +1,8 @@
 let id = 0;
-const PREFIX = __ZENT_VERSION__.replace(/[^0-9a-z]/gi, 'x');
+const PREFIX =
+  'v' +
+  __ZENT_VERSION__.replace(/[^0-9a-z]/gi, 'x') +
+  Math.ceil(Math.random() * 8999 + 1000).toString();
 
 export default function uniqueId(prefix?: string): string {
   const nextId = `${PREFIX}${++id}`;
