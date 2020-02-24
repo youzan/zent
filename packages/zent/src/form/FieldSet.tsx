749164ac757fde526f45dad50566582dfe9fa512
wangjiwei <wangjiwei@youzan.com>
2020-02-24 10:08:42 +0800

    [bug fix] FieldSet: 区分prop的model和view两种模式

--- packages/zent/src/form/FieldSet.tsx
+++ packages/zent/src/form/FieldSet.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {
-  IValidator,
   useFieldSet,
   FormProvider,
   useValue$,
@@ -15,15 +14,13 @@ import {
   IFormFieldViewDrivenProps,
 } from './shared';
 
-export interface IFieldSetProps<T extends Record<string, BasicModel<unknown>>> {
+export interface IFieldSetBaseProps<
+  T extends Record<string, BasicModel<unknown>>
+> {
   /**
    * 表单提交时滚动到错误时的`DOM`元素的`ref`(来自`React.createRef`或`React.useRef`)
    */
   scrollAnchorRef?: React.RefObject<Element>;
-  /**
-   * 校验规则数组，按数组顺序执行，直到所有都通过或者在第一个失败的地方停止
-   */
-  validators?: Array<IValidator<T>>;
   children?: React.ReactNode;
   /**
    * 用于渲染整个 `FieldSet` 层面的错误
@@ -31,14 +28,29 @@ export interface IFieldSetProps<T extends Record<string, BasicModel<unknown>>> {
   renderError?: IRenderError<T>;
 }
 
+export interface IFieldSetModelDrivenProps<
+  T extends Record<string, BasicModel<unknown>>
+> extends IFieldSetBaseProps<T> {
+  model: FieldSetModel<T>;
+}
+
+export interface IFieldSetViewDrivenProps<
+  T extends Record<string, BasicModel<unknown>>
+>
+  extends Omit<IFormFieldViewDrivenProps<T>, 'defaultValue'>,
+    IFieldSetBaseProps<T> {}
+
 export function FieldSet<T extends Record<string, BasicModel<unknown>>>(
-  props: IFieldSetProps<T> &
-    Omit<IFormFieldViewDrivenProps<T>, 'defaultValue'> & {
-      model?: FieldSetModel<T>;
-    }
+  props: IFieldSetModelDrivenProps<T> | IFieldSetViewDrivenProps<T>
 ) {
-  const [ctx, model] = useFieldSet(props.name || props.model, props.validators);
-  const { scrollAnchorRef, renderError = defaultRenderError } = props;
+  const { name, validators } = props as IFieldSetViewDrivenProps<T>;
+  const { model: rawModel } = props as IFieldSetModelDrivenProps<T>;
+  const [ctx, model] = useFieldSet(name || rawModel, validators);
+
+  const {
+    scrollAnchorRef,
+    renderError = defaultRenderError,
+  } = props as IFieldSetBaseProps<T>;
   useFormChild(model as BasicModel<unknown>, scrollAnchorRef);
   useValue$(model.error$, model.error$.getValue());
   return (
