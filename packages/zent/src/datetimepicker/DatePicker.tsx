f2b975790d22001333ed11e25d93b1254b5c7cff
cythiaY <cythia.xy@gmail.com>
2020-02-27 19:05:49 +0800

    fix: 修复日期组件value为对象变量时，内部直接引用赋值的问题

--- packages/zent/src/datetimepicker/DatePicker.tsx
+++ packages/zent/src/datetimepicker/DatePicker.tsx
@@ -155,7 +155,7 @@ export class DatePicker extends PureComponent<IDatePickerProps, any> {
     if (this.isDisabled(val)) return;
 
     // 如果选择的日期和最小日期同一天，则设置时间为最小日期的时间
-    activedTime = setSameDate(activedTime, val);
+    activedTime = setSameDate(val);
     if (min) {
       const minDate = parseDate(min, format);
       if (activedTime < minDate) {
