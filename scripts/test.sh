c0d91f7620dc3d3dc0539294c93309e2b3f0652e
Charles Lee <zombiefml@gmail.com>
2020-08-04 15:11:19 +0800

    Update github workflows

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,11 @@
 set -e
 
 mkdir -p packages/zent/es
-ts-node --project packages/zent/scripts/cruiser/tsconfig.json  packages/zent/scripts/cruiser/index.ts packages/zent/src/index.ts packages/zent/assets
+
+# Don't run in CI
+if [[ -z "${CI}" ]]; then
+  ts-node --project packages/zent/scripts/cruiser/tsconfig.json  packages/zent/scripts/cruiser/index.ts packages/zent/src/index.ts packages/zent/assets
+fi
+
 yarn workspace zent test
 yarn workspace babel-plugin-zent test
