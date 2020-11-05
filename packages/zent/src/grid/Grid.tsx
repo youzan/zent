017eb1a52f1381e16f7651d98b6e536b1021c405
pigwang <wangjiwei@youzan.com>
2020-11-05 11:07:28 +0800

    [bug fix] Grid: 修复自定义Row组件的属性类型 (#1518)
    * [bug fix] Grid: 修复自定义Row组件的属性类型
    
    * chore: 删除没有用的类型

--- packages/zent/src/grid/Grid.tsx
+++ packages/zent/src/grid/Grid.tsx
@@ -58,7 +58,7 @@ function stopPropagation(e: React.MouseEvent) {
 
 const prefix = 'zent';
 
-export interface IGridProps<Data = any> {
+export interface IGridProps<Data = any, RowProps extends object = object> {
   columns: IGridColumn[];
   datasets: Data[];
   rowKey?: string;
@@ -80,9 +80,9 @@ export interface IGridProps<Data = any> {
   ellipsis?: boolean;
   onExpand?: IGridOnExpandHandler<Data>;
   components?: {
-    row?: React.ComponentType;
+    row?: React.ComponentType<RowProps>;
   };
-  rowProps?: (data: Data, index: number) => any;
+  rowProps?: (data: Data, index: number) => RowProps;
   batchRender?: IGridBatchRender;
   stickyBatch?: boolean;
   autoStick?: boolean;
@@ -105,10 +105,10 @@ export interface IGridInnerColumn<Data> extends IGridColumn<Data> {
   key?: string;
 }
 
-export class Grid<Data = any> extends PureComponent<
-  IGridProps<Data>,
-  IGridState
-> {
+export class Grid<
+  Data = any,
+  RowProps extends object = object
+> extends PureComponent<IGridProps<Data, RowProps>, IGridState> {
   static defaultProps: Partial<IGridProps> = {
     className: '',
     bordered: false,
@@ -152,7 +152,7 @@ export class Grid<Data = any> extends PureComponent<
   lastScrollTop!: number;
   stickyHead = React.createRef<HTMLDivElement>();
 
-  constructor(props: IGridProps<Data>) {
+  constructor(props: IGridProps<Data, RowProps>) {
     super(props);
 
     const expandRowKeys = this.getExpandRowKeys(props);
@@ -174,7 +174,7 @@ export class Grid<Data = any> extends PureComponent<
     };
   }
 
-  getExpandRowKeys(props: IGridProps<Data>) {
+  getExpandRowKeys(props: IGridProps<Data, RowProps>) {
     const { expandation, datasets } = props;
     if (expandation) {
       const { isExpanded } = expandation;
@@ -347,7 +347,7 @@ export class Grid<Data = any> extends PureComponent<
   };
 
   getColumns = (
-    props: IGridProps<Data>,
+    props: IGridProps<Data, RowProps>,
     columnsArg?: Array<IGridInnerColumn<Data>>,
     expandRowKeysArg?: boolean[]
   ) => {
@@ -997,7 +997,7 @@ export class Grid<Data = any> extends PureComponent<
 
   // 等重构再删了吧，改不动
   // eslint-disable-next-line react/no-deprecated
-  componentWillReceiveProps(nextProps: IGridProps<Data>) {
+  componentWillReceiveProps(nextProps: IGridProps<Data, RowProps>) {
     if (nextProps.selection?.hasOwnProperty('selectedRowKeys')) {
       this.store.setState({
         selectedRowKeys: nextProps.selection.selectedRowKeys || [],
