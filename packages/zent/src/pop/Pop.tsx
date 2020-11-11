c34afbff5f759812e16e6d564491c8db43ff072f
cythiaY <cythia.xy@gmail.com>
2020-11-11 17:40:29 +0800

    fix: 保留'header'相关类

--- packages/zent/src/pop/Pop.tsx
+++ packages/zent/src/pop/Pop.tsx
@@ -223,7 +223,10 @@ export class Pop extends Component<IPopProps, IPopState> {
         ref={this.popoverRef}
         visible={closePending ? true : visible}
         onVisibleChange={closePending ? noop : onVisibleChange}
-        className={cx('zent-pop', className)}
+        className={cx('zent-pop', className, {
+          'zent-pop--has-header': hasHeader,
+          'zent-pop--no-header': !hasHeader,
+        })}
         style={style}
         cushion={cushion}
         position={getArrowPosition(position, centerArrow)}
