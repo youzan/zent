147732cf121f54104e8bf566d1379213366300ef
cythiaY <cythia.xy@gmail.com>
2020-11-25 16:36:50 +0800

    fix: FormDatePickerField 类型修复

--- packages/zent/src/form/form-components/DateRangePickerField.tsx
+++ packages/zent/src/form/form-components/DateRangePickerField.tsx
@@ -1,23 +1,28 @@
 import * as React from 'react';
 import { Omit } from 'utility-types';
-import { DateRangePicker, IDateRangePickerProps } from '../../date-picker';
+import {
+  DateRangePicker,
+  IDateRangePickerProps,
+  IValueType,
+  RangeDate,
+} from '../../date-picker';
 import { FormField } from '../Field';
 import { IFormComponentProps, dateRangeDefaultValueFactory } from '../shared';
 import { $MergeParams } from '../utils';
-import { RangeDate } from '../../date-picker';
 
-export type IFormDateRangePickerFieldProps = IFormComponentProps<
-  RangeDate,
-  Omit<IDateRangePickerProps, 'value'>
->;
+export type IFormDateRangePickerFieldProps<
+  T extends IValueType
+> = IFormComponentProps<RangeDate, Omit<IDateRangePickerProps<T>, 'value'>>;
 
-export const FormDateRangePickerField: React.FunctionComponent<IFormDateRangePickerFieldProps> = props => {
+export const FormDateRangePickerField = <T extends IValueType = 'string'>(
+  props: IFormDateRangePickerFieldProps<T>
+) => {
   return (
     <FormField
       {...props}
       defaultValue={
-        (props as $MergeParams<IFormDateRangePickerFieldProps>).defaultValue ??
-        dateRangeDefaultValueFactory
+        (props as $MergeParams<IFormDateRangePickerFieldProps<T>>)
+          .defaultValue ?? dateRangeDefaultValueFactory
       }
     >
       {childProps => <DateRangePicker {...props.props} {...childProps} />}
--- packages/zent/src/form/form-components/WeekPickerField.tsx
+++ packages/zent/src/form/form-components/WeekPickerField.tsx
@@ -1,22 +1,27 @@
 import * as React from 'react';
 import { Omit } from 'utility-types';
 import { IFormComponentProps, dateRangeDefaultValueFactory } from '../shared';
-import { WeekPicker, IWeekPickerProps } from '../../date-picker';
+import {
+  WeekPicker,
+  IWeekPickerProps,
+  IValueType,
+  RangeDate,
+} from '../../date-picker';
 import { FormField } from '../Field';
 import { $MergeParams } from '../utils';
-import { RangeDate } from '../../date-picker';
 
-export type IFormWeekPickerFieldProps = IFormComponentProps<
-  RangeDate,
-  Omit<IWeekPickerProps, 'value'>
->;
+export type IFormWeekPickerFieldProps<
+  T extends IValueType
+> = IFormComponentProps<RangeDate, Omit<IWeekPickerProps<T>, 'value'>>;
 
-export const FormWeekPickerField: React.FunctionComponent<IFormWeekPickerFieldProps> = props => {
+export const FormWeekPickerField = <T extends IValueType = 'string'>(
+  props: IFormWeekPickerFieldProps<T>
+) => {
   return (
     <FormField
       {...props}
       defaultValue={
-        (props as $MergeParams<IFormWeekPickerFieldProps>).defaultValue ??
+        (props as $MergeParams<IFormWeekPickerFieldProps<T>>).defaultValue ??
         dateRangeDefaultValueFactory
       }
     >
