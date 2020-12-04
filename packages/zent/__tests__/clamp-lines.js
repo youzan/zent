2c1c3de5ec897ce55279f5ff3de401e63daf0337
Charles Lee <zombiefml@gmail.com>
2020-12-04 22:20:19 +0800

    fix test coverage

--- packages/zent/__tests__/clamp-lines.js
+++ packages/zent/__tests__/clamp-lines.js
@@ -7,24 +7,36 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('ClampLines', () => {
   const content =
     'Zent ( ˈzent ) 是有赞 PC 端 WebUI 规范的 React 实现，提供了一整套基础的 UI 组件以及一些常用的业务组件。目前我们有 50+ 组件，这些组件都已经在有赞的各类 PC 业务中使用，我们会在此基础上，持续开发一些新组件。我们的目标是让 React 项目开发更快、更简单。';
+
   it('basic', () => {
     const wrapper = mount(
       <ClampLines lines={2} popWidth={400} text={content} />
     );
     const instance = wrapper.find(ClampLines).instance();
-    instance.maxHeight = 40;
-    instance.lineHeight = 20;
-    instance.element = {
-      clientHeight: 20,
-    };
-    instance.innerElement.current = {
-      textContent: '',
-    };
+    instance.element = document.createElement('div');
+    instance.element.style.height = '20px';
+    instance.innerElement.current = document.createElement('div');
     instance.clampLines();
-    wrapper.setState({ noClamp: false });
+    wrapper.setState({ holdsFullText: false });
     wrapper.render();
-    expect(wrapper.state().noClamp).toBe(false);
-    expect(instance.innerElement.current.textContent).toBe(content);
+    expect(wrapper.state().holdsFullText).toBe(false);
+    const text = instance.innerElement.current.textContent;
+    expect(text).not.toBe(content);
+    expect(text.endsWith('...')).toBe(true);
+  });
+
+  it('mode = correctness', () => {
+    const wrapper = mount(
+      <ClampLines lines={1} text={content} mode="correctness" />
+    );
+    const instance = wrapper.find(ClampLines).instance();
+    instance.element = document.createElement('div');
+    instance.element.style.height = '20px';
+    instance.innerElement.current = document.createElement('div');
+    wrapper.setState({ holdsFullText: false });
+    instance.clampLines();
+    wrapper.render();
+    expect(wrapper.state().holdsFullText).toBe(true);
   });
 
   it('do not show pop', () => {
@@ -32,10 +44,8 @@ describe('ClampLines', () => {
       <ClampLines lines={2} popWidth={400} text={content} showPop={false} />
     );
     const instance = wrapper.find(ClampLines).instance();
-    instance.innerElement.current = {
-      textContent: '',
-    };
-    instance.state.noClamp = false;
+    instance.innerElement.current = document.createElement('div');
+    instance.state.holdsFullText = false;
     const spy = jest.spyOn(instance, 'renderClampedText');
     instance.render();
     expect(spy).toBeCalled();
