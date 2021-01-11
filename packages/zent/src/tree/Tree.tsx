20eae8e591725f6b377088f9a7c7f55135e36654
chenchenchen <530547479@qq.com>
2021-01-11 14:48:06 +0800

    Update Tree.tsx
    remove useless

--- packages/zent/src/tree/Tree.tsx
+++ packages/zent/src/tree/Tree.tsx
@@ -96,11 +96,10 @@ export class Tree extends Component<ITreeProps, ITreeState> {
       const formatData = createStateByProps(nextProps);
       let { expandNode } = formatData;
 
-      // // 只有在 loadMore 状态下，我会保持原有打开状态
-      // // if (prevProps.loadMore || nextProps.loadMore) {
-      // 任何情况下都做保留
-      expandNode = correctExpand(state, formatData);
-      // // }
+      // 任何情况下都做保留, 除非全展/全闭变化了
+      if (nextProps.expandAll === prevProps.expandAll) {
+        expandNode = correctExpand(state, formatData);
+      }
 
       return {
         prevProps: nextProps,
