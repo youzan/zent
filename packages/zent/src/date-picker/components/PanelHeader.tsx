77920bd7424fc28552d0762fa892464ac2501fef
yuyuehui <m18758428901@163.com>
2021-11-18 15:09:27 +0800

    feat: 去除注释代码

--- packages/zent/src/date-picker/components/PanelHeader.tsx
+++ packages/zent/src/date-picker/components/PanelHeader.tsx
@@ -1,44 +1,11 @@
 import noop from '../../utils/noop';
 import Icon from '../../icon';
 
-// const XMLNS = 'http://www.w3.org/2000/svg';
-// const transformMap = {
-//   left: '',
-//   right: 'matrix(-1 0 0 1 18 0)',
-// };
-// const Path = 'M10 11l-1 1-4-4-1-1 5-5 1 1-4 4 4 4z';
-
 const prefixCls = 'zent-datepicker-panel-header';
 interface IArrowProps {
   onClick: () => void;
   type: 'left' | 'right';
 }
-// const DoubleArrow: React.FC<IArrowProps> = ({ onClick, type }) => (
-//   <svg
-//     width="14"
-//     height="14"
-//     xmlns={XMLNS}
-//     className={`${prefixCls}-arrow`}
-//     onClick={onClick}
-//   >
-//     <g fillRule="evenodd" transform={transformMap[type]}>
-//       <path d={Path} />
-//       <path transform="translate(4)" d={Path} />
-//     </g>
-//   </svg>
-// );
-//
-// const Arrow: React.FC<IArrowProps> = ({ onClick, type }) => (
-//   <svg
-//     width="14"
-//     height="14"
-//     xmlns={XMLNS}
-//     className={`${prefixCls}-arrow`}
-//     onClick={onClick}
-//   >
-//     <path d={Path} transform={transformMap[type]} />
-//   </svg>
-// );
 
 const DoubleIconArrow: React.FC<IArrowProps> = ({ onClick, type }) => (
   <Icon
