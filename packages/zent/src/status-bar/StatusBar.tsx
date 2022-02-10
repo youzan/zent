34593a6f5fb5053efe4a2572773a2718671ae236
vinelink <57426410+vinelink@users.noreply.github.com>
2022-02-10 17:26:32 +0800

    V10 alert (#1863)
    不再建议直接使用Alert
    - ✨  支持自定义左侧 Icon
    - ✨  支持自定义 `close` 按钮颜色
    - ✨  支持配置顶部进度条进度
    - ✨ 新增 `StatusBar`、`Prompt`、`Banner` 组件
    
    ## break change
    - ✨  支持配置 `bordered` 调整边框是否显示，默认无边框

---
+++ packages/zent/src/status-bar/StatusBar.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react';
+import cx from 'classnames';
+import { IAlertProps, Alert } from '../alert';
+
+export type StatusBarType = 'info' | 'waiting' | 'success' | 'error';
+
+export interface IStatusBarProps extends Omit<IAlertProps, 'type'> {
+  type?: StatusBarType;
+}
+
+const TypePropsMap: Record<StatusBarType, Partial<IAlertProps>> = {
+  info: {
+    type: 'info',
+  },
+  waiting: {
+    type: 'info',
+    loading: true,
+  },
+  success: {
+    type: 'success',
+  },
+  error: {
+    type: 'error',
+  },
+};
+
+export const StatusBar: FC<IStatusBarProps> = ({
+  type = 'info',
+  progress,
+  className,
+  closable = true,
+  ...rest
+}) => {
+  const typeProps = TypePropsMap[type];
+  const taskProgress = type === 'waiting' ? progress : 0;
+  const statusBarClassName = cx('zent-status-bar', className);
+
+  return (
+    <Alert
+      className={statusBarClassName}
+      {...typeProps}
+      {...rest}
+      closable={closable}
+      progress={taskProgress}
+    />
+  );
+};
+export default StatusBar;
